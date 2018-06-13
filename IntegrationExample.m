1ad66bd8155af5d106a0a8f115d3de0617c1598f
Fred J Hickernell <hickernell@iit.edu>
2018-06-13 12:19:18 +1000

    documented

--- IntegrationExample.m
+++ IntegrationExample.m
@@ -1,15 +1,15 @@
 %% Example
-stopObj = CLTStopping
-distribObj = IIDDistribution;
+stopObj = CLTStopping %stopping criterion
+distribObj = IIDDistribution; %IID sampling with uniform distribution
 [sol, out] = integrate(KeisterFun, distribObj, stopObj)
-stopObj.absTol = 1e-3;
+stopObj.absTol = 1e-3; %decrease tolerance
 [sol, out] = integrate(KeisterFun, distribObj, stopObj)
 
-distribObj.trueDistribution = 'normal';
-stopObj.absTol = 0.02;
+distribObj.trueDistribution = 'normal'; %Change to normal distribution
+stopObj.absTol = 0.01; %increase tolerance
 OptionObj = AsianCallFun(4) %4 time steps
 [sol, out] = integrate(OptionObj, distribObj, stopObj)
 OptionObj = AsianCallFun(64) %single level, 64 time steps
 [sol, out] = integrate(OptionObj, distribObj, stopObj)
-OptionObj = AsianCallFun([4 4 4]) %multilevel, 64 time steps
+OptionObj = AsianCallFun([4 4 4]) %multilevel, 64 time steps, faster
 [sol, out] = integrate(OptionObj, distribObj, stopObj)
