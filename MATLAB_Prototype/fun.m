97a421ba9632dfa002fe1f3337d21a1c0f0377bf
Sou-Cheng Choi <schoi32@iit.edu>
2018-08-06 20:18:28 -0500

    Take away a space.

--- MATLAB_Prototype/fun.m
+++ MATLAB_Prototype/fun.m
@@ -9,7 +9,7 @@ properties
 end
 
 methods (Abstract)
-	y = f(obj, xu, coordIndex)
+   y = f(obj, xu, coordIndex)
    % xu = nodes, §\mcommentfont $\vx_{\fu,i} = i^{\text{th}}$ row of an $n \times |\fu|$ matrix§
    % coordIndex = set of those coordinates in sequence needed, §\mcommentfont $\fu$§
    % y = §\mcommentfont$n \times p$ matrix with values $f(\vx_{\fu,i},\vc)$ where if $\vx_i' = (x_{i,\fu},\vc)_j$, then $x'_{ij} = x_{ij}$ for $j \in \fu$, and $x'_{ij} = c$ otherwise§
