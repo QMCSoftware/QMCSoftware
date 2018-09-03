48a621414fec1f829a9a9c97b4980c88978b8750
Sou-Cheng Choi <schoi32@iit.edu>
2018-09-03 15:51:12 -0500

    Debugging dt_AsianCallFun.py (not done)

--- Python_Prototype/AsianCallFun.py
+++ Python_Prototype/AsianCallFun.py
@@ -4,44 +4,45 @@ from numpy.linalg import eig
 from scipy.sparse import spdiags
 
 class AsianCallFun(fun):
-    volatility = 0.5
-    S0 = 30
-    K = 25
-    T = 1
-    A = []
-    tVec = []
-    dimFac = 1
 
     # Specify and generate payoff values of an Asian Call option
     def __init__(self, dimFac = []):
-        super().__init__()
-        if dimFac != []:
-            dimVec = cumprod(dimFac, axis=0)
-            nf = dimVec.size
-            self = [AsianCallFun() for i in range(nf)]
-            self(0).dimFac = 0
+        self.volatility = 0.5
+        self.S0 = 30
+        self.K = 25
+        self.T = 1
+        self.A = []
+        self.tVec = []
+        if dimFac == []:
+            self.dimFac = 1
+        else:
+            self.dimVec = cumprod(dimFac, axis=0)
+            nf = self.dimVec.size
+            acf_array = [AsianCallFun() for i in range(nf)]
+            acf_array[0].dimFac = 0
             for ii in range(nf):
-                d = dimVec(ii)
+                d = self.dimVec(ii)
                 if ii > 0:
-                    self[ii].dimFac = dimFac(ii - 1)
-                self(ii).dimension = d
-                tvec = range(d) * (self[ii].T / d)
-                self(ii).tVec = tvec
+                    acf_array[ii].dimFac = dimFac(ii - 1)
+                acf_array[ii].dimension = d
+                tvec = range(d) * (acf_array[ii].T / d)
+                acf_array(ii).tVec = tvec
                 CovMat = min(transpose(tvec),tvec)
                 [eigVec, eigVal] = eig(CovMat, 'vector')
-                self[ii].A = multiply(sqrt(eigVal(:-1:1)), transpose(eigVec(:, :-1:1)))
-        return self
+                acf_array[ii].A = multiply(sqrt(eigVal[-1:-1:1]), transpose(eigVec[:,-1:-1:1]))
 
+            self = acf_array
+        super().__init__()
 
     def f(self, x, coordIndex):
         # since the nominalValue = 0, this is efficient
-        BM = x * self.A
+        BM = multiply(x, self.A)
         SFine = self.S0 * exp((-self.volatility ^ 2 / 2) * self.tVec + self.volatility * BM)
-        AvgFine = ((self.S0 / 2) + sum(SFine(:, 1:self.dimension-1), 2) + SFine(:, self.dimension) / 2) / self.dimension
+        AvgFine = ((self.S0 / 2) + sum(SFine[:, 1:self.dimension-1], 2) + SFine[:, self.dimension] / 2) / self.dimension
         y = max(AvgFine - self.K, 0)
         if self.dimFac > 0:
-            SCoarse = SFine(:, self.dimFac: self.dimFac:end)
+            SCoarse = SFine[:, self.dimFac: self.dimFac:-1]
             dCoarse = self.dimension / self.dimFac
-            AvgCoarse = ((self.S0 / 2) + sum(SCoarse(:, 1:dCoarse-1), 2) + SCoarse(:, dCoarse) / 2) / dCoarse
+            AvgCoarse = ((self.S0 / 2) + sum(SCoarse[:, 1:dCoarse-1], 2) + SCoarse[:, dCoarse] / 2) / dCoarse
             y = y - max(AvgCoarse - self.K, 0)
         return y
--- Python_Prototype/Tests/dt_AsianCallFun.py
+++ Python_Prototype/Tests/dt_AsianCallFun.py
@@ -1,8 +1,16 @@
 """
 >>> from AsianCallFun import AsianCallFun as AsianCallFun
->>> acf = AsianCallFun() # doctest:+ELLIPSIS
->>> acf.dimension
+>>> acf = AsianCallFun()
+>>> acf.volatility
+0.5
+>>> acf.S0
+30
+>>> acf.K
+25
+>>> acf.T
 1
+>>> acf.dimension
+2
 >>> acf.distribType
 'uniform'
 >>> acf.domainType
@@ -12,7 +20,8 @@
 >>> acf.domain
 array([[0, 0],
        [1, 1]])
->>> import numpy as np; acf.f(np.array([[1, 2], [3, 4]]), [1, 2])
-array([-4.15915193e-03,  3.93948451e-12])
 
+>>> import numpy as np
+>>> acf.f(np.array([[1, 2], [3, 4]]), [1, 2])
+array([-4.15915193e-03,  3.93948451e-12])
 """
