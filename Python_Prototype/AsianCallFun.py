18bd691fde57b891ea0d10da68603c3cdc83d52d
alegresor <agsorokin3@gmail.com>
2018-10-10 11:53:29 -0500

    Alternate Possible AsianCallFun.py implementation

--- Python_Prototype/AsianCallFun.py
+++ Python_Prototype/AsianCallFun.py
@@ -46,3 +46,110 @@ class AsianCallFun(fun):
             AvgCoarse = ((self.S0 / 2) + sum(SCoarse[:, 1:dCoarse-1], 2) + SCoarse[:, dCoarse] / 2) / dCoarse
             y = y - max(AvgCoarse - self.K, 0)
         return y
+
+'''
+from fun import fun as fun
+import numpy as np
+
+def cumprod_m(a):
+    r,_ = a.shape
+    if r != 1:
+        return np.cumprod(a,axis = 0)
+    return np.cumprod(a,axis = 1)
+
+def min_m(a):
+    b = np.eye(len(a))
+    for i in range(len(a)):
+        for j in range(len(a)):
+            b[i,j] = min(a[i],a[j])
+    return b
+
+
+
+class AsianCall(fun): # Translated up to the comment "Left off here"
+    def __init__(self,dimFac=None): # Can pass in dimFac as float, int, list, or numpy.ndarray. Will convert to numpy array regardless
+        super().__init__()
+        
+        self.volatility = 0.5
+        self.S0 = 30
+        self.K = 25
+        self.T = 1
+        self.A = None
+        self.tVec = None
+        self.dimFac = 1
+        self.dimFac = dimFac
+        
+        if self.dimFac == None:
+            return
+
+        self.dimFac = dimFac
+        if type(dimFac) == list:
+            self.dimFac == np.asarray(dimFac).reshape(1,len(dimFac))
+        elif dimFac == float or dimFac == int:
+            self.dimFac = self.asArray([dimFac])
+        else:
+            raise Exception("AsianCall is constructed with a float, int, list, or (preferably) numpy.ndarray")
+
+        self.dimVec = cumprod_m(self.dimFac)
+        nf = len(dimVec)
+
+        self.obj_list = list(range(nf))
+        for x in range(nf):
+            self.obj_list[x] = AsianCallFun()
+        self.obj_list[0].dimFac = 0    
+        
+        for ii in range(nf):
+            d = self.dimVec[ii]
+            if ii > 0:
+                self.obj_list[ii].dimFac = self.dimFac[ii-1]
+            self.obj_list[ii].dimension = d
+            tvec = np.arange(1,1+d)*(obj[ii].T/d)
+            self.obj_list[ii].tVec = tvec
+                     
+            # Left off here
+            try:
+                CovMat = min_g(tvec)
+             
+            except: # Try block should never throw exception with how min_g is created
+                #CovMat = min(tvec'*ones(1,length(tvec)), ones(length(tvec),1)*tvec);
+                raise Exception("Problem taking min(tvec',tvec). Exception not yet implemented")
+            
+            # Left off here
+            [eigVec,eigVal] = eig(CovMat,'vector') # np.linalg.eig is not the sae as Matlab's eig function
+
+            #Example of dissimilarity
+            #    Matlab:
+            #        a = (1:4)
+            #        b = min(a',a)
+            #        [t1,t2] = eig(b,'vector') 
+            #    Python:
+            #        import numpy as np
+            #        a = np.array([[1,1,1,1],[1,2,2,2],[1,2,3,3],[1,2,3,4]])
+            #        t1,t2 = np.linalg.eig(a)     
+            
+            try:
+                self.obj_list[ii].A = np.sqrt(eigVal(end:-1:1)) .* eigVec(:,end:-1:1)';
+            except:
+                v = np.sqrt(eigVal(end:-1:1));
+                n = len(v);
+                self.obj_list[ii].A = (v * np.ones(1,n)) .* eigVec(:,end:-1:1)';
+
+    def f(self, obj, x, coordIndex): # None of this function has been translated yet
+        # Since the nominalValue = 0, this is efficient
+        
+        BM = x * obj.A
+        try:
+            SFine = obj.S0 * np.exp((-1* obj.volatility**2/2)*obj.tVec + obj.volatility * BM)
+        except:
+            n, ~ = BM.shape
+            SFine = obj.S0*np.exp((-obj.volatility^2/2)*np.ones(n,1)*obj.tVec + obj.volatility * BM);
+        AvgFine = ((obj.S0/2) + sum(SFine(:,1:obj.dimension-1),2) + SFine(:,obj.dimension)/2)/obj.dimension;
+        y = max(AvgFine - obj.K,0);
+        if obj.dimFac > 0
+            SCoarse = SFine(:,obj.dimFac:obj.dimFac:end);
+            dCoarse = obj.dimension/obj.dimFac;
+            AvgCoarse = ((obj.S0/2) + sum(SCoarse(:,1:dCoarse-1),2) + SCoarse(:,dCoarse)/2)/dCoarse;
+            y = y - max(AvgCoarse - obj.K,0);
+        return y      
+            
+'''
