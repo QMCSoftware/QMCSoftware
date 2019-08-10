4e1ebaedce26a155fb522009f89e87186db145ba
Sou-Cheng Choi <schoi32@iit.edu>
2019-08-10 14:29:58 -0500

    Add spacing for readability

--- Presentations+Papers/AmBCP_2019/Latex/Keister.snip.py
+++ Presentations+Papers/AmBCP_2019/Latex/Keister.snip.py
@@ -1,9 +1,10 @@
 dim = 2
-stopObj = CLTStopping(nInit=16,absTol=.5)
+stopObj = CLTStopping(nInit=16, absTol=.5)
 measureObj = measure().IIDZMeanGaussia(
-    dimension=[dim],variance=[.5])
+    dimension=[dim], variance=[.5])
 distribObj = IIDDistribution(
     trueD=measure().stdGaussian(
         dimension=[dim]))
-sol,out = integrate(KeisterFun(),
-    measureObj,distribObj,stopObj)
+sol, out = integrate(KeisterFun(),
+              measureObj, distribObj,
+              stopObj)
