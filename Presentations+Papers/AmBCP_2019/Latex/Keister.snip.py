da3ca7bebea19eacb4d372a35067441971d54366
Sou-Cheng Choi <schoi32@iit.edu>
2019-08-11 16:54:08 -0500

    Poster: (1) Put figure before code (2) Change dim to d in code.

--- Presentations+Papers/AmBCP_2019/Latex/Keister.snip.py
+++ Presentations+Papers/AmBCP_2019/Latex/Keister.snip.py
@@ -1,10 +1,11 @@
-dim = 2
-stopObj = CLTStopping(nInit=16, absTol=.5)
-measureObj = measure().IIDZMeanGaussia(
-    dimension=[dim], variance=[.5])
+d = 2
+stopObj = CLTStopping(nInit=16,
+                      absTol=.5)
+measureObj = measure().\
+    IIDZMeanGaussian(dimension=[d],
+                     variance=[.5])
 distribObj = IIDDistribution(
     trueD=measure().stdGaussian(
-        dimension=[dim]))
+        dimension=[d]))
 sol, out = integrate(KeisterFun(),
-              measureObj, distribObj,
-              stopObj)
+    measureObj, distribObj, stopObj)
