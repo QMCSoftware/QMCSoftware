a7f8f506deb3e645e92c380ff37029826dba6145
Sou-Cheng Choi <schoi32@iit.edu>
2019-09-28 15:41:02 -0500

    Make PEP 8 compatible.

--- qmcpy/algorithms/__init__.py
+++ qmcpy/algorithms/__init__.py
@@ -1,20 +1,25 @@
-''' Originally developed in MATLAB by Fred Hickernell. Translated to python by Sou-Cheng T. Choi and Aleksei Sorokin '''
+''' Originally developed in MATLAB by Fred Hickernell. Translated to python by
+Sou-Cheng T. Choi and Aleksei Sorokin '''
 
-def univ_repr(self,objList_s=None):
+
+def univ_repr(self, objList_s=None):
     '''
     clean way to represent object data
     note: print(obj) == print(obj.__repr__())
     '''
-    s = '%s object with properties:\n'%(type(self).__name__)
-    for key,val in self.__dict__.items():
+    s = '%s object with properties:\n' % (type(self).__name__)
+    for key, val in self.__dict__.items():
         if str(key) != objList_s:
-            s += '%4s%s: %s\n'%('',str(key),str(val).replace('\n','\n%15s'%('')))
-    if not objList_s: return s[:-1]
+            s += '%4s%s: %s\n' % \
+                 ('', str(key), str(val).replace('\n', '\n%15s' % ('')))
+    if not objList_s:
+        return s[:-1]
     # print list of subObject with properties
-    s += '    %s:\n'%(objList_s)
-    for i,subObj in enumerate(self):
-        s += '%8s%s[%d] with properties:\n'%('',objList_s,i)
-        for key,val in subObj.__dict__.items():
+    s += '    %s:\n' % (objList_s)
+    for i, subObj in enumerate(self):
+        s += '%8s%s[%d] with properties:\n' % ('', objList_s, i)
+        for key, val in subObj.__dict__.items():
             if str(key) != objList_s:
-                s += '%12s%s: %s\n'%('',str(key),str(val).replace('\n','\n%20s'%('')))
+                s += '%12s%s: %s\n' % \
+                     ('', str(key), str(val).replace('\n', '\n%20s' % ('')))
     return s[:-1]
--- qmcpy/algorithms/integrate.py
+++ qmcpy/algorithms/integrate.py
@@ -1,9 +1,11 @@
-''' Originally developed in MATLAB by Fred Hickernell. Translated to python by Sou-Cheng T. Choi and Aleksei Sorokin '''
+''' Originally developed in MATLAB by Fred Hickernell. Translated to python by
+Sou-Cheng T. Choi and Aleksei Sorokin '''
 from time import time
 
-def integrate(funObj,measureObj,distribObj,stopObj):
+
+def integrate(funObj, measureObj, distribObj, stopObj):
     '''
-    Specify and generate values $f(x)$ for $x \in \cx$
+    Specify and generate values $f(x)$ for $x \in \mathcal{X}$
 
     :param funObj: an object from class fun
     :param measureObj: an object from class maeasure
@@ -14,9 +16,10 @@ def integrate(funObj,measureObj,distribObj,stopObj):
     # Initialize the accumData object and other crucial objects
     funObj = funObj.transformVariable(measureObj, distribObj)
     stopObj.stopYet(funObj)
-    while stopObj.dataObj.stage != 'done': # the dataObj.stage property tells us where we are in the process
-        stopObj.dataObj.updateData(distribObj,funObj) # compute additional data
-        stopObj.stopYet(funObj) # update the status of the computation
+    while stopObj.dataObj.stage != 'done':
+        # the dataObj.stage property tells us where we are in the process
+        stopObj.dataObj.updateData(distribObj, funObj)  # compute more data
+        stopObj.stopYet(funObj)  # update the status of the computation
     solution = stopObj.dataObj.solution  # assign outputs
     stopObj.dataObj.timeUsed = time() - stopObj.dataObj._timeStart
     return solution, stopObj.dataObj
