673ac07bf63596156b386423391ef26dd0a50fd2
Sou-Cheng Choi <schoi32@iit.edu>
2019-09-29 10:00:50 -0500

    Better documentation for files in algorithms.

--- qmcpy/algorithms/__init__.py
+++ qmcpy/algorithms/__init__.py
@@ -2,24 +2,32 @@
 Sou-Cheng T. Choi and Aleksei Sorokin '''
 
 
-def univ_repr(self, objList_s=None):
-    '''
-    clean way to represent object data
-    note: print(obj) == print(obj.__repr__())
-    '''
+def univ_repr(self, obj_list_s=None) -> str:
+    """
+    Clean way to represent object data.
+    Note: print(obj) == print(obj.__repr__())
+
+    Args:
+        self: an object instance
+        obj_list_s:
+
+    Returns:
+        str
+
+    """
     s = '%s object with properties:\n' % (type(self).__name__)
     for key, val in self.__dict__.items():
-        if str(key) != objList_s:
+        if str(key) != obj_list_s:
             s += '%4s%s: %s\n' % \
                  ('', str(key), str(val).replace('\n', '\n%15s' % ('')))
-    if not objList_s:
+    if not obj_list_s:
         return s[:-1]
     # print list of subObject with properties
-    s += '    %s:\n' % (objList_s)
+    s += '    %s:\n' % (obj_list_s)
     for i, subObj in enumerate(self):
-        s += '%8s%s[%d] with properties:\n' % ('', objList_s, i)
+        s += '%8s%s[%d] with properties:\n' % ('', obj_list_s, i)
         for key, val in subObj.__dict__.items():
-            if str(key) != objList_s:
+            if str(key) != obj_list_s:
                 s += '%12s%s: %s\n' % \
                      ('', str(key), str(val).replace('\n', '\n%20s' % ('')))
     return s[:-1]
--- qmcpy/algorithms/integrate.py
+++ qmcpy/algorithms/integrate.py
@@ -3,23 +3,23 @@ Sou-Cheng T. Choi and Aleksei Sorokin '''
 from time import time
 
 
-def integrate(funObj, measureObj, distribObj, stopObj):
+def integrate(fun_obj, measure_obj, distrib_obj, stop_obj):
     '''
     Specify and generate values $f(x)$ for $x \in \mathcal{X}$
 
-    :param funObj: an object from class fun
-    :param measureObj: an object from class maeasure
-    :param distribObj: an object from class discrete_distribution
-    :param stopObj:  an object from class stopping_criterion
+    :param fun_obj: an object from class fun
+    :param measure_obj: an object from class maeasure
+    :param distrib_obj: an object from class discrete_distribution
+    :param stop_obj:  an object from class stopping_criterion
     :return: None
     '''
     # Initialize the accumData object and other crucial objects
-    funObj = funObj.transformVariable(measureObj, distribObj)
-    stopObj.stopYet(funObj)
-    while stopObj.dataObj.stage != 'done':
+    fun_obj = fun_obj.transformVariable(measure_obj, distrib_obj)
+    stop_obj.stopYet(fun_obj)
+    while stop_obj.dataObj.stage != 'done':
         # the dataObj.stage property tells us where we are in the process
-        stopObj.dataObj.updateData(distribObj, funObj)  # compute more data
-        stopObj.stopYet(funObj)  # update the status of the computation
-    solution = stopObj.dataObj.solution  # assign outputs
-    stopObj.dataObj.timeUsed = time() - stopObj.dataObj._timeStart
-    return solution, stopObj.dataObj
+        stop_obj.dataObj.updateData(distrib_obj, fun_obj)  # compute more data
+        stop_obj.stopYet(fun_obj)  # update the status of the computation
+    solution = stop_obj.dataObj.solution  # assign outputs
+    stop_obj.dataObj.timeUsed = time() - stop_obj.dataObj._timeStart
+    return solution, stop_obj.dataObj
