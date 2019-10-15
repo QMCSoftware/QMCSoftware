3655570e62d96d770a515ae6b0089040be387412
alegresor <agsorokin3@gmail.com>
2019-10-15 18:19:41 -0500

    workout for scatter plotting iid and quasi-random

---
+++ python_prototype/workouts/wo_plot_discrete_distribs.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qmcpy.discrete_distribution import IIDStdUniform,IIDStdGaussian,Lattice,Sobol
+
+n = 100
+
+def iid_scatters():
+    fig,ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    for i,(distrib,color,lims) in enumerate(
+            zip([IIDStdUniform(rng_seed=7),IIDStdGaussian(rng_seed=7)],['b','r'],[[0,1],[-3,3]])): 
+        samples = distrib.gen_samples(1,n,2).squeeze()
+        ax[i].scatter(samples[:,0],samples[:,1],color=color)
+        ax[i].set_xlabel('$x_0$')
+        ax[i].set_ylabel('$x_1$')
+        ax[i].set_xlim(lims), ax[i].set_ylim(lims)
+    ax[0].set_title('Standard Uniform')
+    ax[1].set_title('Standard Gaussian')
+    fig.suptitle('IID Nodes')
+    plt.show()
+    fig.savefig('outputs/scatters_iid.png',dpi=200)
+
+def lds_scatters():
+    fig,ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    for i,(distrib,color) in enumerate(
+            zip([Lattice(rng_seed=7),Sobol(rng_seed=7)],['g','c'])): 
+        samples = distrib.gen_samples(1,n,2).squeeze()
+        ax[i].scatter(samples[:,0],samples[:,1],color=color)
+        ax[i].set_xlabel('$x_0$')
+        ax[i].set_ylabel('$x_1$')
+        ax[i].set_xlim([0,1]), ax[i].set_ylim([0,1])
+    ax[0].set_title('Lattice')
+    ax[1].set_title('Sobol')
+    fig.suptitle('Randomly Shifted Low-Discrepancy Sequences')
+    plt.show()
+    fig.savefig('outputs/scatters_lds.png',dpi=200)
+
+if __name__ == '__main__':
+    iid_scatters()
+    lds_scatters()
