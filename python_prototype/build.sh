98230a0791054bc9e91489aca142d468fe7421b2
Sou-Cheng Choi <schoi32@iit.edu>
2019-10-16 21:09:25 -0500

    Refine autopep8 and pylint commands in build.sh

--- python_prototype/build.sh
+++ python_prototype/build.sh
@@ -14,10 +14,11 @@ echo "Qmcpy build process starts..."
 echo "$(date)"
 
 # autopep8
-autopep8 . --in-place --recursive --ignore E402,E241 --global-config ./setup.cfg
+autopep8 . --in-place --recursive --ignore E402,E701 --exclude ./qmcpy/third_party/magic_point_shop --global-config ./setup.cfg
 
 # pylint
-pylint **/*.py
+pylint **/.py
+pylint **/**/*.py
 
 # Uninstall and Install Qmcpy
 pip uninstall --yes qmcpy
