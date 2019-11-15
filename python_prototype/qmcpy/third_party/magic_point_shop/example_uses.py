a287b4a5d09eadc1f102a0ef502142475d455c6c
alegresor <agsorokin3@gmail.com>
2019-11-14 23:36:30 -0600

    update notebooks act as intro to QMCPy

--- python_prototype/qmcpy/third_party/magic_point_shop/example_uses.py
+++ python_prototype/qmcpy/third_party/magic_point_shop/example_uses.py
@@ -1,18 +1,19 @@
 from time import time
 from numpy import *
 
-from . import LatticeSeq, DigitalSeq
+from qmcpy.third_party.magic_point_shop import LatticeSeq
+from qmcpy.discrete_distribution import DigitalSeq
 
 def gen_lattice_points(n,d):
     t0 = time()
-    lattice_rng = LatticeSeq(m=20,s=d)
+    lattice_rng = LatticeSeq(m=30,s=d)
     x = array([next(lattice_rng) for i in range(n)])
     print('Lattice Time: %.3f'%(time()-t0))
     return x
 
 def gen_sobol_points(n,d):
     t0 = time()
-    sobol_rng = DigitalSeq(Cs="sobol_Cs.col", m=20, s=d)
+    sobol_rng = DigitalSeq(Cs="sobol_Cs.col", m=30, s=d)
     x = zeros((n, d), dtype=int64)
     for i in range(n):
         next(sobol_rng)
@@ -20,7 +21,7 @@ def gen_sobol_points(n,d):
     print('Sobol Time: %.3f'%(time()-t0))
     return x
 
-n = 2**15
-d = 50
+n = 2**21
+d = 1
 gen_lattice_points(n,d)
 gen_sobol_points(n,d)
