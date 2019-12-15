2c551109fffe030a676ed723ee2233937f7e9d5e
Sou-Cheng Choi <schoi32@iit.edu>
2019-12-15 16:33:18 -0600

    Introduce loop to get non-zero time.

--- python_prototype/workouts/wo_lds_sequences/matlab_sequences.m
+++ python_prototype/workouts/wo_lds_sequences/matlab_sequences.m
@@ -6,17 +6,22 @@ lattice_shift = rand(1,dim);
 n_2powers = (1:20)';
 lattice_times = n_2powers;
 sobol_times = n_2powers;
+repeat = 40;
 for i=1:size(n_2powers,1)
     % Lattice
     t=cputime;
-    x_lat = gail.lattice_gen(1,2^i,dim);
-    x_lat_shifted = mod(x_lat+lattice_shift,1);
-    lattice_times(i) = cputime-t;
+    for j=1:repeat
+      x_lat = gail.lattice_gen(1,2^i,dim);
+      x_lat_shifted = mod(x_lat+lattice_shift,1);
+    end
+    lattice_times(i) = (cputime-t)/repeat;
     % Sobol
     t=cputime;
-    sob = scramble(sobolset(dim),'MatousekAffineOwen');
-    x_Sobol_scrambled = net(sob,2^i);
-    sobol_times(i) = cputime-t;
+    for j=1:repeat
+      sob = scramble(sobolset(dim),'MatousekAffineOwen');
+      x_Sobol_scrambled = net(sob,2^i);
+    end
+    sobol_times(i) = (cputime-t)/repeat;
 end
 
 results = [n_2powers, lattice_times, sobol_times]
