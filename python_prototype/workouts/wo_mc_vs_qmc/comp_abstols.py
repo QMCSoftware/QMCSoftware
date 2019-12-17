1d6e2ead5776d7121dc5228cb2872c65101c5fd6
alegresor <agsorokin3@gmail.com>
2019-12-17 15:13:13 -0600

    rework mc_vs_qmc notebook w/ repeated trials

--- python_prototype/workouts/wo_mc_vs_qmc/comp_abstols.py
+++ python_prototype/workouts/wo_mc_vs_qmc/comp_abstols.py
@@ -3,10 +3,11 @@
 from qmcpy import *
 
 import json
-from numpy import nan
+from numpy import nan, zeros
 from pandas import DataFrame
 
 distribution_pointers = [IIDStdUniform, IIDStdGaussian, Lattice, Sobol]
+trials = 3
 
 def abstol_comparison(abstols=arange(.1, .4, .1)):
     """
@@ -24,31 +25,36 @@ def abstol_comparison(abstols=arange(.1, .4, .1)):
     for i, abs_tol in enumerate(abstols):
         row_i = {'abs_tol': abs_tol}
         for distrib_pointer in distribution_pointers:
-            distribution = distrib_pointer(rng_seed=7)
-            integrand = Keister(dimension=dimension)
-            measure = Gaussian(dimension=dimension, variance=1 / 2)
-            distrib_name = type(distribution).__name__
-            if distrib_name in ['IIDStdGaussian', 'IIDStdUniform']:
-                stopping_criterion = CLT(distribution, measure, abs_tol=abs_tol,
-                                         n_max=1e10, n_init=256)
-            elif distrib_name in ['Lattice', 'Sobol']:
-                stopping_criterion = CLTRep(distribution, measure, abs_tol=abs_tol,
-                                            n_max=1e10, n_init=16)
-            try:
-                sol, data = integrate(integrand, measure, distribution, stopping_criterion)
-                time = data.time_total
-                n = data.n_total
-            except:
-                sol, time, n = nan, nan, nan
-            row_i[distrib_name + '_solution'] = sol
-            row_i[distrib_name + '_time'] = time
-            row_i[distrib_name + '_n'] = n
+            for j in range(trials):
+                sols, times, ns = zeros(trials), zeros(trials), zeros(trials)
+                distribution = distrib_pointer(rng_seed=7)
+                integrand = Keister(dimension=dimension)
+                measure = Gaussian(dimension=dimension, variance=1 / 2)
+                distrib_name = type(distribution).__name__
+                if distrib_name in ['IIDStdGaussian', 'IIDStdUniform']:
+                    stopping_criterion = CLT(distribution, measure, abs_tol=abs_tol,
+                                            n_max=1e10, n_init=256)
+                elif distrib_name in ['Lattice', 'Sobol']:
+                    stopping_criterion = CLTRep(distribution, measure, abs_tol=abs_tol,
+                                                n_max=1e10, n_init=32)
+                try:
+                    sol, data = integrate(integrand, measure, distribution, stopping_criterion)
+                    sols[j] = sol
+                    times[j] = data.time_total
+                    ns[j] = data.n_total
+                except:
+                    sols[j] = nan
+                    times[j] = nan
+                    ns[j] = nan
+            row_i[distrib_name + '_solution'] = sols.mean()
+            row_i[distrib_name + '_time'] = times.mean()
+            row_i[distrib_name + '_n'] = ns.mean()
         print(row_i)
         df.loc[i] = row_i
     return df
 
 if __name__ == '__main__':
     # Absolute Tolerance Comparison Test
-    abstols = arange(.0008, .1002, .0002)
+    abstols = arange(.001, .0502, .0002)
     df_abstols = abstol_comparison(abstols)
     df_abstols.to_csv('outputs/mc_vs_qmc/abs_tol.csv', index=False)
--- python_prototype/workouts/wo_mc_vs_qmc/comp_dimensions.py
+++ python_prototype/workouts/wo_mc_vs_qmc/comp_dimensions.py
@@ -3,10 +3,11 @@
 from qmcpy import *
 
 import json
-from numpy import nan
+from numpy import nan, zeros
 from pandas import DataFrame
 
 distribution_pointers = [IIDStdUniform, IIDStdGaussian, Lattice, Sobol]
+trials = 3
 
 def dimension_comparison(dimensions=arange(1, 4, 1)):
     """
@@ -22,25 +23,30 @@ def dimension_comparison(dimensions=arange(1, 4, 1)):
     for i, dimension in enumerate(dimensions):
         row_i = {'dimension': dimension}
         for distrib_pointer in distribution_pointers:
-            distribution = distrib_pointer(rng_seed=7)
-            integrand = Keister(dimension=dimension)
-            measure = Gaussian(dimension=dimension, variance=1 / 2)
-            distrib_name = type(distribution).__name__
-            if distrib_name in ['IIDStdGaussian', 'IIDStdUniform']:
-                stopping_criterion = CLT(distribution, measure, rel_tol=.01, abs_tol=0,
-                                         n_max=1e10, n_init=256)
-            elif distrib_name in ['Lattice', 'Sobol']:
-                stopping_criterion = CLTRep(distribution, measure, rel_tol=.01, abs_tol=0,
-                                            n_max=1e10, n_init=16)
-            try:
-                sol, data = integrate(integrand, measure, distribution, stopping_criterion)
-                time = data.time_total
-                n = data.n_total
-            except:
-                sol, time, n = nan, nan, nan
-            row_i[distrib_name + '_solution'] = sol
-            row_i[distrib_name + '_time'] = time
-            row_i[distrib_name + '_n'] = n
+            for j in range(trials):
+                sols, times, ns = zeros(trials), zeros(trials), zeros(trials)
+                distribution = distrib_pointer(rng_seed=7)
+                integrand = Keister(dimension=dimension)
+                measure = Gaussian(dimension=dimension, variance=1 / 2)
+                distrib_name = type(distribution).__name__
+                if distrib_name in ['IIDStdGaussian', 'IIDStdUniform']:
+                    stopping_criterion = CLT(distribution, measure, rel_tol=.01, abs_tol=0,
+                                            n_max=1e10, n_init=256)
+                elif distrib_name in ['Lattice', 'Sobol']:
+                    stopping_criterion = CLTRep(distribution, measure, rel_tol=.01, abs_tol=0,
+                                                n_max=1e10, n_init=32)
+                try:
+                    sol, data = integrate(integrand, measure, distribution, stopping_criterion)
+                    sols[j] = sol
+                    times[j] = data.time_total
+                    ns[j] = data.n_total
+                except:
+                    sols[j] = nan
+                    times[j] = nan
+                    ns[j] = nan
+                row_i[distrib_name + '_solution'] = sols.mean()
+            row_i[distrib_name + '_time'] = times.mean()
+            row_i[distrib_name + '_n'] = ns.mean()
         print(row_i)
         df.loc[i] = row_i
     return df
