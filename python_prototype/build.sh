377c745faacc461af52d8b2fafed6824574e53ea
Sou-Cheng Choi <schoi32@iit.edu>
2019-12-28 08:30:23 -0600

    Enhance build.sh

--- python_prototype/build.sh
+++ python_prototype/build.sh
@@ -17,34 +17,44 @@ export PYTHONPATH=$PYTHONPATH:"$(pwd)"
 echo $PYTHONPATH
 
 pwd
-
+################################################################################
 # autopep8
+################################################################################
 autopep8 . --in-place --recursive --ignore E402,E701,E501,E128,C0413 --exclude ./third_party
 # pylint
 pylint --variable-rgx="[a-z0-9_]{1,30}$" --disable W0622,C0103,C0321,W0614 **/*.py
 pylint --variable-rgx="[a-z0-9_]{1,30}$" --disable W0622,C0103,C0321,W0614 **/**/*.py
 
+################################################################################
 # Uninstall and Install Qmcpy
+################################################################################
 pip uninstall --yes qmcpy
 python setup.py install
 python setup.py clean
 
+################################################################################
 # Run unit tests
+################################################################################
 #    May need to run "conda install python.app" once for pythonw
 #    to be in the same virutal environment as python.
 pythonw -m unittest discover -s test/fasttests
 pythonw -m unittest discover -s test/longtests
 
+################################################################################
 # Use sphinx to generate HTML documentation from above and Python docstrings
+################################################################################
 cd sphinx
 ./autodoc.sh
 cd ..
 
+################################################################################
 # git commands
-
+################################################################################
 git add -f ../docs
 
+################################################################################
 # Check time stamps
+################################################################################
 ls -ltr .
 
 ls -ltr outputs
