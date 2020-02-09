619a7282b24d12002f66d67d1dc1184b5324c3e2
alegresor <agsorokin3@gmail.com>
2020-02-09 12:44:51 -0600

    longtests passing

--- python_prototype/test/longtests/test_integrate.py
+++ python_prototype/test/longtests/test_integrate.py
@@ -1,9 +1,8 @@
 """ Unit tests for integrate method in QMCPy """
 
-import unittest
-
-from numpy import arange, array
 from qmcpy import *
+from numpy import arange, array
+import unittest
 
 
 class IntegrationExampleTest(unittest.TestCase):
@@ -15,68 +14,62 @@ class IntegrationExampleTest(unittest.TestCase):
         2D:  N[Integrate[E^(-x1^2 - x2^2) Cos[Sqrt[x1^2 + x2^2]], {x1, -Infinity, Infinity}, {x2, -Infinity, Infinity}]]
         1D:  N[Integrate[E^(-x^2) Cos[Sqrt[x^2]], {x, -Infinity, Infinity}]]
         """
-        abs_tol = 0.01
+        abs_tol = .01
         dimensions = [1, 2, 3]
         true_values = [1.3803884470431430, 1.808186429263620, 2.168309102165481]
-        for d in dimensions:
-            integrand = Keister(dimension=d)
-            distrib = IIDStdGaussian(rng_seed=7)
-            measure = Gaussian(dimension=d, variance=1 / 2)
-            stopping_criterion = CLT(distrib, measure,
-                                     abs_tol=abs_tol)
-            sol, _ = integrate(integrand, measure,
-                               distrib, stopping_criterion)
-            true_value = true_values[d - 1]
-            self.assertTrue(abs(sol - true_value) < abs_tol)
+        for i in range(len(dimensions)):
+            distribution = IIDStdGaussian(dimension=dimensions[i])
+            measure = Gaussian(distribution, variance=1/2)
+            integrand = Keister(measure)
+            stopper = CLT(distribution,abs_tol=abs_tol)
+            solution,data = integrate(stopper,integrand,measure,distribution)
+            self.assertTrue(abs(solution - true_values[i]) < abs_tol)
 
     def test_asian_option_multi_level(self):
-        abstol = 0.1
-        time_vec = [arange(1 / 4, 5 / 4, 1 / 4),
+        abs_tol = .01
+        time_vector = [arange(1 / 4, 5 / 4, 1 / 4),
                     arange(1 / 16, 17 / 16, 1 / 16),
                     arange(1 / 64, 65 / 64, 1 / 64)]
-        dims = [len(tv) for tv in time_vec]
-        distrib = IIDStdGaussian(rng_seed=7)
-        measure = BrownianMotion(dims, time_vector=time_vec)
-        integrand = AsianCall(measure)
-        stopping_criterion = CLT(distrib, measure, abs_tol=abstol)
-        sol, _ = integrate(integrand, measure,
-                           distrib, stopping_criterion)
+        levels = 3
+        distributions = MultiLevelConstructor(levels,
+        IIDStdGaussian,
+            dimension = [len(tv) for tv in time_vector],
+            seed = 7)
+        measures = MultiLevelConstructor(levels,
+            BrownianMotion,
+                distribution = distributions,
+                time_vector = time_vector)
+        integrands = MultiLevelConstructor(levels,
+            AsianCall,
+                measure = measures)
+        stopper = CLT(distributions, abs_tol=abs_tol)
+        solution,data = integrate(stopper, integrands, measures, distributions)
         true_value = 6.20
-        self.assertTrue(abs(sol - true_value) < abstol)
+        self.assertTrue(abs(solution - true_value) < abs_tol)
 
     def test_lebesgue_measure(self):
         """ Mathematica: Integrate[x^3 y^3, {x, 1, 3}, {y, 3, 6}] """
         abs_tol = 1
         dimension = 2
-        integrand = QuickConstruct(dimension=dimension,
-                                   custom_fun=lambda x: (x.prod(1)) ** 3)
-        measure = Lebesgue(dimension=dimension,
-                                lower_bound=[array([1, 3])],
-                                upper_bound=[array([3, 6])])
-        distrib = Lattice(rng_seed=7)
-        stopping_criterion = CLTRep(distrib, measure,
-                                    abs_tol=abs_tol)
-        sol, _ = integrate(integrand, measure,
-                           distrib, stopping_criterion)
+        distribution = Lattice(dimension=2, replications=16, scramble=True, backend='MPS')
+        measure = Lebesgue(distribution, lower_bound=[1,3], upper_bound=[3,6])
+        integrand = QuickConstruct(measure, lambda x: (x.prod(1))**3)
+        stopper = CLTRep(distribution, abs_tol=abs_tol)
+        solution,data = integrate(stopper, integrand, measure, distribution)
         true_value = 6075
-        self.assertTrue(abs(sol - true_value) < abs_tol)
+        self.assertTrue(abs(solution - true_value) < abs_tol)
 
     def test_uniform_measure(self):
         """ Mathematica: Integrate[(x^3 y^3)/6, {x, 1, 3}, {y, 3, 6}] """
         abs_tol = 1
         dimension = 2
-        integrand = QuickConstruct(dimension=dimension,
-                                   custom_fun=lambda x: (x.prod(1)) ** 3)
-        measure = Uniform(dimension=dimension,
-                               lower_bound=[array([1, 3])],
-                               upper_bound=[array([3, 6])])
-        distrib = Lattice(rng_seed=7)
-        stopping_criterion = CLTRep(distrib, measure,
-                                    abs_tol=abs_tol)
-        sol, _ = integrate(integrand, measure,
-                           distrib, stopping_criterion)
+        distribution = Lattice(dimension=2, replications=16, scramble=True, backend='MPS')
+        measure = Uniform(distribution, lower_bound=[1,3], upper_bound=[3,6])
+        integrand = QuickConstruct(measure, lambda x: (x.prod(1))**3)
+        stopper = CLTRep(distribution, abs_tol=abs_tol)
+        solution,data = integrate(stopper, integrand, measure, distribution)
         true_value = 6075 / 6
-        self.assertTrue(abs(sol - true_value) < abs_tol)
+        self.assertTrue(abs(solution - true_value) < abs_tol)
 
     def test_linear(self):
         """ Mathematica:
@@ -84,18 +77,16 @@ class IntegrationExampleTest(unittest.TestCase):
         2D: Integrate[x+y, {x, 0, 1}, {y, 0, 1}]
         3D: Integrate[x+y+z, {x, 0, 1}, {y, 0, 1}, {z, 0, 1}]
         """
-        abs_tol = 0.01
+        abs_tol = .01
         dimensions = [1, 2, 3]
         true_values = [0.5, 1, 1.5]
-        for d in dimensions:
-            integrand = Linear(dimension=d)
-            measure = Uniform(dimension=d)
-            distrib = IIDStdUniform(rng_seed=7)
-            stopping_criterion = CLT(distrib, measure, abs_tol=abs_tol)
-            sol, _ = integrate(integrand, measure, distrib,
-                               stopping_criterion)
-            true_value = true_values[d - 1]
-            self.assertTrue(abs(sol - true_value) < abs_tol)
+        for i in range(len(dimensions)):
+            distribution = Sobol(dimension=dimensions[i], replications=16, scramble=True, backend='MPS')
+            measure = Uniform(distribution)
+            integrand = Linear(measure)
+            stopper = CLTRep(distribution, abs_tol=abs_tol)
+            solution,data = integrate(stopper, integrand, measure, distribution)
+            self.assertTrue(abs(solution - true_values[i]) < abs_tol)
 
     def test_quick_construct(self):
         """
@@ -106,68 +97,36 @@ class IntegrationExampleTest(unittest.TestCase):
         2D: Integrate[5(x+y), {x, 0, 1}, {y, 0, 1}]
         3D: Integrate[5(x+y+z), {x, 0, 1}, {y, 0, 1}, {z, 0, 1}]
         """
-
-        def f(x): return (5 * x).sum(1)
-
+        abs_tol = .01
         dimensions = [1, 2, 3]
         true_values = [2.5, 5, 7.5]
-        for d in dimensions:
-            integrand = QuickConstruct(dimension=d, custom_fun=f)
-            measure = Uniform(dimension=d)
-            sol, data = integrate(integrand, measure)
-            print(data)
-            abs_tol = data.stopping_criterion.abs_tol
-            true_value = true_values[d - 1]
-            self.assertTrue(data.measure.dimension == d)
-            self.assertTrue(abs(sol - true_value) < abs_tol)
+        for i in range(len(dimensions)):
+            distribution = IIDStdUniform(dimension=dimensions[i])
+            measure = Uniform(distribution)
+            integrand = QuickConstruct(measure, lambda x: (5*x).sum(1))
+            stopper = MeanMC_g(distribution, abs_tol=abs_tol)
+            solution,data = integrate(stopper, integrand, measure, distribution)
+            self.assertTrue(abs(solution - true_values[i]) < abs_tol)
 
     def test_quick_construct2(self):
-        """
-        Infer integrand dimension from true measure's
-
-        Mathematica:
-        1D: Integrate[5x, {x, 0, 1}]
-        2D: Integrate[5(x+y), {x, 0, 1}, {y, 0, 1}]
-        3D: Integrate[5(x+y+z), {x, 0, 1}, {y, 0, 1}, {z, 0, 1}]
-        """
-
-        def f(x): return (5 * x).sum(1)
-
-        dimensions = [1, 2, 3]
-        true_values = [2.5, 5, 7.5]
-        for d in dimensions:
-            integrand = QuickConstruct(dimension=d,
-                                       custom_fun=f)
-            measure = Uniform(dimension=d)
-            sol, data = integrate(integrand, measure)
-            print(data)
-            abs_tol = data.stopping_criterion.abs_tol
-            true_value = true_values[d - 1]
-            self.assertTrue(measure.dimension == d)
-            self.assertTrue(data.measure.dimension == d)
-            self.assertTrue(abs(sol - true_value) < abs_tol)
-
-    def test_quick_construct3(self):
         """
         Test integrands with parameters
 
         Mathematica: integrate[b*(x-a)^2, {x,1,0}]
         """
-        dimension = 2
+        abs_tol = .01
         a_list = [1, 2]
         b_list = [4, 5]
-        f_list = [QuickConstruct(
-            dimension=dimension,
-            custom_fun=lambda x, a=a, b=b: b * (x - a) ** 2)
-            for a, b in zip(a_list, b_list)]
-        measure = Uniform(dimension=dimension)
-        sol_data_list = [integrate(f, measure) for f in f_list]
-        sols = [sol_data[0] for sol_data in sol_data_list]
-        true_sols = [(b / 3) * (3 * a * (a - 1) + 1)
-                     for a, b in zip(a_list, b_list)]
-        abs_tol = sol_data_list[0][1].stopping_criterion.abs_tol
-        max_error = max(abs(array(sols) - array(true_sols)))
-        self.assertTrue(max_error < abs_tol)
+        true_values = [(b / 3) * (3 * a * (a - 1) + 1) for a, b in zip(a_list, b_list)]
+        for i in range(2):
+            a_i = a_list[i]
+            b_i = b_list[i]
+            distribution = Lattice(dimension=1, replications=0, scramble=True, backend='GAIL')
+            measure = Uniform(distribution)
+            integrand = QuickConstruct(measure, lambda x, a=a_i, b=b_i: b * (x - a) ** 2)
+            stopper = CubLattice_g(distribution, abs_tol=abs_tol)
+            solution,data = integrate(stopper, integrand, measure, distribution)
+            self.assertTrue(abs(solution - true_values[i]) < abs_tol)
 
 
 if __name__ == "__main__":
