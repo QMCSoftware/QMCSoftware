d44cdbe6760891388a23cdaccf12dd061f09c593
Sou-Cheng Choi <schoi32@iit.edu>
2020-02-23 12:00:57 -0600

    Pytorch installation in autodoc keeps breaking due to excessive memory ussage. We will mock torch documentation whenever it happens.

--- python_prototype/sphinx/conf.py
+++ python_prototype/sphinx/conf.py
@@ -61,6 +61,7 @@ extensions = ["sphinx.ext.autodoc",
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+autodoc_mock_imports = ["torch"]
 
 # Napoleon settings
 napoleon_google_docstring = True
