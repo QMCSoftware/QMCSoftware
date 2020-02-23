a8fef710293a7028e5ada662649718d730a21758
Sou-Cheng Choi <schoi32@iit.edu>
2020-02-23 12:38:19 -0600

    Fix doc for numpy

--- python_prototype/sphinx/conf.py
+++ python_prototype/sphinx/conf.py
@@ -80,7 +80,7 @@ napoleon_use_keyword = True
 # intersphinx
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.7/", None),
-    "numpy": ("https://docs.scipy.org/doc/numpy-1.17.0/reference/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy-1.17.0/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy-1.3.1/reference/", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None)
 }
