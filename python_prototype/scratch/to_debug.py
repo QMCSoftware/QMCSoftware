64e66d0b5dc800917bb536afe95a062b69b52b2b
alegresor <agsorokin3@gmail.com>
2020-05-11 20:50:31 -0500

    cubature_data error

--- python_prototype/scratch/to_debug.py
+++ python_prototype/scratch/to_debug.py
@@ -2,7 +2,7 @@ from qmcpy import *
 from numpy import *
 
 # Probably Brownian Motion or Asian Call problem
-
+'''
 dimension = 16
 tv = arange(1/dimension,1+1/dimension,1/dimension)
 distribution = Lattice(dimension, replications=16, seed=7)
@@ -12,23 +12,13 @@ integrand = AsianCall(measure)
 algorithm = CLTRep(integrand,abs_tol=.001)
 solution,data = algorithm.integrate()
 print(data)
-
+'''
 # CubLattice_g (parallel to matlab) (stilde error. Same with CubSobol_g)
 '''
 distribution = Lattice(dimension=2, scramble=True, replications=0, seed=7, backend='GAIL')
 measure = Uniform(distribution)
 integrand = QuickConstruct(measure, lambda x: 5*x.sum(1))
 algorithm = CubLattice_g(integrand, abs_tol=1e-4, check_cone=True)
-solution,data = algorithm.integrate()
-print(solution)
-'''
-
-# CubSobol_g (parallel to matlab) (stilde error. Same with CubSobol_g)
-'''
-distribution = Sobol(dimension=2, scramble=True, replications=0, seed=7, backend='GAIL')
-measure = Uniform(distribution)
-integrand = QuickConstruct(measure, lambda x: 5*x.sum(1))
-algorithm = CubSobol_g(integrand, abs_tol=1e-4, check_cone=True)
 solution,data = algorithm.integrate()
 print(solution)
 '''
--- python_prototype/workouts/integration_examples/pi_problem.py
+++ python_prototype/workouts/integration_examples/pi_problem.py
@@ -8,10 +8,11 @@ from numpy import *
 from time import perf_counter
 
 t0 = perf_counter()
-distribution = Lattice(dimension=1, replications=16, scramble=True, seed=7, backend='MPS')
+abs_tol = 1e-7 # 2.5e-10
+distribution = Sobol(dimension=1, seed=7, backend='MPS')
 measure = Lebesgue(distribution, lower_bound=-2, upper_bound=2)
 integrand = QuickConstruct(measure, lambda x: sqrt(4 - x**2) * (1 / 2 + x**3 * cos(x / 2)))
-solution,data = CLTRep(integrand, abs_tol=2.5e-10, n_max=2**30).integrate()
+solution,data = CubSobol_g(integrand, abs_tol=abs_tol, n_max=2**30).integrate()
 password = str(solution).replace('.', '')[:10]
 print("Password:", password)  # 3141592653
 print('CPU time: %.2f sec' % (perf_counter() - t0))  # around 30 seconds
