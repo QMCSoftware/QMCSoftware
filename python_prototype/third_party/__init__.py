41f3cc277b380a6f63acd6034e9bfef1603e48ee
alegresor <agsorokin3@gmail.com>
2020-05-19 20:00:18 -0500

    add qrngpy to master

--- python_prototype/third_party/__init__.py
+++
@@ -1,1 +0,0 @@
-from .magic_point_shop import *
--- python_prototype/workouts/lds_sequences/python_sequences.py
+++ python_prototype/workouts/lds_sequences/python_sequences.py
@@ -1,7 +1,6 @@
 """ Record times for QMCPy's quasi-random generators """
 
 from qmcpy import *
-from third_party.magic_point_shop import digitalseq_b2g
 from time import time
 from numpy import *
 from pandas import DataFrame
@@ -12,7 +11,7 @@ def python_sequences(powers_2=arange(1, 4), trials=1, dimension=1):
     Record time for generating samples from each discrete distribution
     """
     print('\nDiscrete DiscreteDistribution Generation Time Comparison')
-    columns = ['n', 'L_MPS_t', 'L_GAIL_t', 'S_QRNG_t', 'S_MPS_OG_t', 'S_MPS_QMCPy_t', 'S_PyTorch_t']
+    columns = ['n', 'L_MPS_t', 'L_GAIL_t', 'S_QRNG_t', 'S_MPS_QMCPy_t', 'S_PyTorch_t']
     df = DataFrame(columns=columns, dtype=float)
     for i, m in enumerate(powers_2):
         n = 2**m
@@ -35,16 +34,6 @@ def python_sequences(powers_2=arange(1, 4), trials=1, dimension=1):
             distribution = Sobol(dimension, scramble=False, seed=7, backend='QRNG')
             distribution.gen_samples(n_min=0,n_max=n)
         row_i['S_QRNG_t'] = (time() - t0) / trials
-        # Sobol Magic Point Shop OG
-        t0 = time()
-        for trial in range(trials):
-            s = digitalseq_b2g(
-                Cs="./third_party/magic_point_shop/sobol_Cs.col", m=30, s=dimension, returnDeepCopy=True)
-            x = zeros((n, dimension), dtype=int64)
-            for i in range(n):
-                next(s)
-                x[i, :] = s.cur
-        row_i['S_MPS_OG_t'] = (time() - t0) / trials
         # Sobol Magic Point Shop QMCPy
         t0 = time()
         for trial in range(trials):
