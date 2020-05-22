942b1c026af8548e489fdbdfca05c64b6352086f
alegresor <agsorokin3@gmail.com>
2020-05-22 15:50:35 -0500

    minor spelling/citation changes

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 [Homepage](https://qmcsoftware.github.io/QMCSoftware/) | [GitHub Repository](https://github.com/QMCSoftware/QMCSoftware)
 
+<hr>
+
 # Quasi-Monte Carlo Community Software
 
 Quasi-Monte Carlo (QMC) methods are used to approximate multivariate integrals. They have four main components: an integrand, a discrete distribution, summary output data, and stopping criterion. Information about the integrand is obtained as a sequence of values of the function sampled at the data-sites of the discrete distribution. The stopping criterion tells the algorithm when the user-specified error tolerance has been satisfied. We are developing a framework that allows collaborators in the QMC community to develop plug-and-play modules in an effort to produce more efficient and portable QMC software. Each of the above four components is an abstract class. Abstract classes specify the common properties and methods of all subclasses. The ways in which the four kinds of classes interact with each other are also specified. Subclasses then flesh out different integrands, sampling schemes, and stopping criteria. Besides providing developers a way to link their new ideas with those implemented by the rest of the QMC community, we also aim to provide practitioners with state-of-the-art QMC software for their applications. 
@@ -56,6 +58,8 @@ make doc_html
 make doc_pdf
 make doc_epub
 ~~~
+
+<hr>
 
 ## QMCPy
 
@@ -121,7 +125,7 @@ python -W ignore -m unittest discover -s test/longtests
 
 If you find QMCPy helpful in your work, please support us by citing the following work:
 
-Fred J. Hickernell, Sou-Cheng T. Choi, and Aleksei Sorokin, “QMC  Community Software.” Python software, 2019. Work in progress. Available from [https://github.com/QMCSoftware/QMCSoftware](https://github.com/QMCSoftware/QMCSoftware).
+ Choi, S.-C. T., Hickernell, F. J., McCourt, M., Rathinavel, J. & Sorokin,A. QMCPy: A quasi-Monte Carlo Python Library. Working. 2020. [https://qmcsoftware.github.io/QMCSoftware/](https://qmcsoftware.github.io/QMCSoftware/).
 
 This work is maintained under the Apache 2.0 License.
 
--- setup.py
+++ setup.py
@@ -29,21 +29,19 @@ class CleanCommand(Command):
     def run(self):
         os.system("rm -vrf ./build ./dist ./*.pyc ./qmcpy/qmcpy.egg-info")
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = "Quasi-Monte Carlo (QMC) methods are used to approximate multivariate integrals. They have five main components: an integrand, a true measure, a discrete distribution, a stopping criterion, and summary output data. Information about the integrand is obtained as a sequence of values of the function sampled at the data-sites of the discrete distribution. The stopping criterion tells the algorithm when the user-specified error tolerance has been satisfied. We are developing a framework that allows collaborators in the QMC community to develop plug-and-play modules in an effort to produce more efficient and portable QMC software. Each of the above five components is an abstract class. Abstract classes specify the common properties and methods of all subclasses. The ways in which the five kinds of classes interact with each other are also specified. Subclasses then flesh out different integrands, sampling schemes, and stopping criteria. Besides providing developers a way to link their new ideas with those implemented by the rest of the QMC community, we also aim to provide practitioners with state-of-the-art QMC software for their applications."
 
 setuptools.setup(
     name="qmcpy",
     version="0.1",
-    author="Fred Hickernell, Aleksei Sorokin, and Sou-Cheng T. Choi",
-    author_email="hickernell@iit.com",
+    author="Fred Hickernell, Sou-Cheng T. Choi, Mike McCourt, Jagadeeswaran Rathinavel, Aleksei Sorokin",
+    author_email="asorokin@hawk.iit.edu",
     description="(Quasi) Monte Carlo Framework in Python 3.6 and 3.7",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text",
     url="https://github.com/QMCSoftware/QMCSoftware",
     package_dir={"": "qmcpy"},
-    packages=setuptools.find_packages("qmcpy"),
+    packages=setuptools.find_packages('qmcpy'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: IIT License",
