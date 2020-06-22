8a6a8ec001a2a01a54a573461dfc7d769e9c8a01
alegresor <agsorokin3@gmail.com>
2020-06-22 16:39:52 -0500

    start masters thesis figs

---
+++ writeups/sorokin_masters_thesis/tm1.py
@@ -0,0 +1,17 @@
+
+from qmcpy import *
+mu = [3.,2.]
+sigma = [[9. , 5.],
+         [5. , 4.]]
+dd = Sobol(2, seed=7) # discrete distribution
+tm = Gaussian(dd, mean=mu, covariance=sigma) # true measure
+x = tm.gen_mimic_samples(2**7)
+
+from matplotlib import pyplot
+pyplot.scatter(x[:,0],x[:,1],color='c')
+pyplot.xlim([-6.,12.])
+pyplot.xticks([-6.,0.,12.])
+pyplot.ylim([-2.,6.])
+pyplot.yticks([-2.,2.,6.])
+pyplot.tight_layout()
+pyplot.savefig('writeups/sorokin_masters_thesis/figs/tm1.png',dpi=250)
