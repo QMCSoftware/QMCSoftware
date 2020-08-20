db4df4a1d927af3fe3f5873c373f87455ba82cd8
Aleksei Sorokin <agsorokin3@gmail.com>
2020-08-19 20:32:07 -0500

    remove duplicate plot from discrete distrib

--- qmcpy/discrete_distribution/_discrete_distribution.py
+++ qmcpy/discrete_distribution/_discrete_distribution.py
@@ -52,58 +52,6 @@ class DiscreteDistribution(object):
         """
         raise MethodImplementationError(self, 'set_seed')
 
-    def plot(self, dim_x=0, dim_y=1, n=2**7, point_size=5, color='c', show=True, out=None):
-        """
-        Make a scatter plot from samples. Requires dimension >= 2.
-
-        Args:
-            dim_x (int): index of first dimension to be plotted on horizontal axis.
-            dim_y (int): index of the second dimension to be plotted on vertical axis.
-            n (int): number of samples to draw as self.gen_samples(n)
-            point_size (int): ax.scatter(...,s=point_size)
-            color (str): ax.scatter(...,color=color)
-            show (bool): show plot or not?
-            out (str): file name to output image. If None, the image is not output
-
-        Return:
-            tuple: fig,ax from `fig,ax = matplotlib.pyplot.subplots(...)`
-        """
-        if self.dimension < 2:
-            raise ParameterError('Plotting a discrete distribution requires dimension >=2. ')
-        x = self.gen_samples(n)
-        from matplotlib import pyplot
-        pyplot.rc('font', size=16)
-        pyplot.rc('legend', fontsize=16)
-        pyplot.rc('figure', titlesize=16)
-        pyplot.rc('axes', titlesize=16, labelsize=16)
-        pyplot.rc('xtick', labelsize=16)
-        pyplot.rc('ytick', labelsize=16)
-        if self.mimics == 'StdUniform':
-            fig,ax = pyplot.subplots(figsize=(5,5))
-            ax.set_xlim([0,1])
-            ax.set_ylim([0,1])
-            ax.set_xticks([0,1])
-            ax.set_yticks([0,1])
-            ax.set_aspect(1)
-        elif self.mimics == 'StdGaussian':
-            fig,ax = pyplot.subplots(figsize=(5,5))
-            ax.set_xlim([-3,3])
-            ax.set_ylim([-3,3])
-            ax.set_xticks([-3,3])
-            ax.set_yticks([-3,3])
-            ax.set_aspect(1)
-        else:
-            fig,ax = pyplot.subplots()
-        ax.set_xlabel('$x_{i,%d}$'%dim_x)
-        ax.set_ylabel('$x_{i,%d}$'%dim_y)
-        ax.scatter(x[:,dim_x],x[:,dim_y],color=color,s=point_size)
-        s = '$2^{%d}$'%log2(n) if log2(n)%1==0 else '%d'%n
-        ax.set_title(s+' %s Samples'%type(self).__name__)
-        fig.tight_layout()
-        if out: pyplot.savefig(out,dpi=250)
-        if show: pyplot.show()
-        return fig,ax
-
     def __repr__(self):
         return _univ_repr(self, "DiscreteDistribution", self.parameters)
 
