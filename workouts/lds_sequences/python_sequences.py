5d6309613a1e204c226543d76932bfd564f13d58
alegresor <agsorokin3@gmail.com>
2021-01-15 10:38:08 -0600

    remove torch dependency

--- workouts/lds_sequences/python_sequences.py
+++ workouts/lds_sequences/python_sequences.py
@@ -5,8 +5,6 @@ from time import time
 from numpy import *
 from pandas import DataFrame
 import warnings
-import torch
-
 
 def python_sequences(powers_2=arange(1, 4), trials=1, dimension=1):
     """
@@ -17,7 +15,7 @@ def python_sequences(powers_2=arange(1, 4), trials=1, dimension=1):
         'L_natural', 'L_linear', 'L_MPS',
         'H_QRNG', 'H_Owen',
         'K_QRNG',
-        'S_QMCPy', 'S_PyTorch']
+        'S_QMCPy']
     warnings.simplefilter('ignore')
     dds = [
         Lattice(dimension, randomize=True, order='natural', seed=7),
@@ -31,20 +29,13 @@ def python_sequences(powers_2=arange(1, 4), trials=1, dimension=1):
     for i, m in enumerate(powers_2):
         n = 2**m
         row_i = {'n': n}
-        for j in range(len(columns)-2):
+        for j in range(len(columns)-1):
             dd_name = columns[j+1]
             dd = dds[j]
             t0 = time()
             for trial in range(trials):
                 x = dd.gen_samples(n)
             row_i[dd_name] = (time() - t0) / trials
-        # pytorch
-        s = torch.quasirandom.SobolEngine(dimension,scramble=True,seed=7)
-        t0 = time()
-        for trial in range(trials):
-            s.reset()
-            x = s.draw(int(n),dtype=torch.float64).numpy()
-        row_i['S_PyTorch'] = (time() - t0) / trials
         df.loc[i] = row_i
         print('\n'.join(['%s: %.4f'%(key,val) for key,val in row_i.items()])+'\n')
     return df
