c77489bd82fce071b6b3e6fe854b7fea60b32a9d
CDHJ2000 <38597916+CDHJ2000@users.noreply.github.com>
2021-07-11 16:02:52 -0700

    Update LR.py

--- LR.py
+++ LR.py
@@ -1,9 +1,14 @@
-from qmcpy.integrand._integrand import Integrand
-from qmcpy import Sobol, Uniform, CubQMCSobolG, CubMCCLT
+from qmcpy.true_measure.uniform import Uniform
+from ._integrand import Integrand
+from ..discrete_distribution import Sobol
+from ..true_measure import Gaussian, Lebesgue
+from ..discrete_distribution._discrete_distribution import DiscreteDistribution
+from ..true_measure._true_measure import TrueMeasure
+from ..util import ParameterError
 import numpy
-from qmcpy import *
+from numpy.linalg import norm as norm
 
-class LogisticRegression(Integrand):
+class LR(Integrand):
     def __init__(self, sampler, s_matrix, t):
         self.true_measure = Uniform(sampler)
         m, d = s_matrix.shape
@@ -23,38 +28,22 @@ class LogisticRegression(Integrand):
         else:
             print("for all 't_i', 0 <= t_i <= 1")
         
-        super(LogisticRegression, self).__init__()
+        super(LR, self).__init__()
         
     def g(self, x):
-        product = 1
         m, d = self.s.shape
         a, b = x.shape
+        values = []
         for c in range(0, a):
+            product = 1
             for i in range(0, m):
                 total = x[c][0]
                 for j in range(1, d+1):
-                    total  = (x[c][j]* self.s[i][j-1]) + total
+                    total  = (x[c][j] * self.s[i][j-1]) + total
                 value1 = numpy.exp(total)
                 value2 = value1 / (1 + value1)
                 product = product * (value2)**(self.t[i])
                 product = product * (1 - value2)**(1-self.t[i])
-                return product
-
-s = numpy.array([[0]])
-t = numpy.array([1])
-
-no, dim = s.shape
-
-k = LogisticRegression(IIDStdUniform(dim+1,seed=7), s_matrix = s, t = t)
-solution, data = CubMCCLT(k, abs_tol = .00001).integrate()
-print(solution)
-
-k1 = LogisticRegression(Sobol(dim,seed=7), s_matrix = s, t = t)
-#print(k.f(numpy.array([[0, 1/2, 1]]).T))
-solution1, data1 = CubQMCSobolG(k1, abs_tol = .00001).integrate()
-print(solution1)
-
-my_instance = LogisticRegression(sampler = Sobol(dimension=dim+1, seed = 7), s_matrix = s, t = t)
-p = my_instance.discrete_distrib.gen_samples(n_min=0, n_max=1024)
-y = my_instance.f(p)
-print(y.mean())
+            values = values + [product]
+        values = numpy.array(values)
+        return values
