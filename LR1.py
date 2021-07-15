99e160d6940a8e29ef4a68f45142fb5a0578cc53
CDHJ2000 <cdhjr2000@gmail.com>
2021-07-14 21:04:08 -0700

    Logistic Regression

--- LR1.py
+++ LR1.py
@@ -4,20 +4,21 @@ import numpy
 from numpy.linalg import norm as norm
 from qmcpy import *
 
-s = numpy.array([[0], [0]])
-t = numpy.array([1, 1])
+s = numpy.array([[0]])
+t = numpy.array([1])
 
 no, dim = s.shape
 
-k = LR(IIDStdUniform(dim+1,seed=8), s_matrix = s, t = t)
-solution, data = CubMCCLT(k, abs_tol = .001).integrate()
-print(data)
+#k = LR(IIDStdUniform(dim+1,seed=8), s_matrix = s, t = t)
+#solution, data = CubMCCLT(k, abs_tol = .001).integrate()
+#print(data)
 
-k1 = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t)
-solution1, data1 = CubQMCSobolG(k1, abs_tol = .001).integrate()
-print(data1)
+#k1 = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t)
+#solution1, data1 = CubQMCSobolG(k1, abs_tol = .001).integrate()
+#print(data1)
 
 my_instance = LR(sampler = Sobol(dimension=dim+1, seed = 7), s_matrix = s, t = t)
 p = my_instance.discrete_distrib.gen_samples(n_min=0, n_max=1024)
 y = my_instance.f(p)
 print(y.mean())
+print(my_instance.true_measure)
