0a93bc56032d6694f3de479f4207a34a084f8f6b
alegresor <agsorokin3@gmail.com>
2021-07-26 13:50:07 -0500

    reformat *.{c,h} files + .editorconfig

--- qmcpy/discrete_distribution/c_lib/MRG63k3a.h
+++ qmcpy/discrete_distribution/c_lib/MRG63k3a.h
@@ -1,2 +1,2 @@
-double MRG63k3a (void);
-void seed_MRG63k3a (long long);
+double MRG63k3a(void);
+void seed_MRG63k3a(long long);
--- qmcpy/discrete_distribution/c_lib/korobov_qrng.c
+++ qmcpy/discrete_distribution/c_lib/korobov_qrng.c
@@ -17,7 +17,6 @@ References:
 #include "export_ctypes.h"
 #include "MRG63k3a.h"
 
-
 /**
  * @title Generate n Points of a d-dimensional Korobov Sequence
  * @param n number of points
@@ -31,37 +30,45 @@ References:
  */
 EXPORT void korobov_qrng(int n, int d, int *generator, int randomize, double *res, long long seed)
 {
-	int i, j, ij;
-	double U;
-	double *aux;
-	aux = (double *) calloc(d, sizeof(double));
-	seed_MRG63k3a(seed);
+    int i, j, ij;
+    double U;
+    double *aux;
+    aux = (double *)calloc(d, sizeof(double));
+    seed_MRG63k3a(seed);
 
-	/* Init */
-	for(j=0; j<d; j++){
-		aux[j] = generator[j] / ((double) n);
-		res[j*n] = 0.0; /* case i = 0 below */
-	}
+    /* Init */
+    for (j = 0; j < d; j++)
+    {
+        aux[j] = generator[j] / ((double)n);
+        res[j * n] = 0.0; /* case i = 0 below */
+    }
 
-	/* Generate points */
-	for(i=1; i<n; i++){ /* omit i=0 as done in init above */
-		for(j=0; j<d; j++){
-			ij = j*n+i;
-			res[ij] = res[j*n + (i-1)] + aux[j];
-			if(res[ij] > 1) res[ij] = res[ij] - 1.0;
-		}
-	}
+    /* Generate points */
+    for (i = 1; i < n; i++)
+    { /* omit i=0 as done in init above */
+        for (j = 0; j < d; j++)
+        {
+            ij = j * n + i;
+            res[ij] = res[j * n + (i - 1)] + aux[j];
+            if (res[ij] > 1)
+                res[ij] = res[ij] - 1.0;
+        }
+    }
 
-	/* Randomization */
-	if(randomize == 1) {
-		for(j=0; j<d; j++){
-			U = MRG63k3a(); /* 63 bit U(0,1) random number */ 
-			for(i=0; i<n; i++){
-				ij = j*n+i;
-				res[ij] = res[ij] + U;
-				if(res[ij] > 1) res[ij] = res[ij] - 1.0;
-			}
-		}
-	}
-	free(aux);
+    /* Randomization */
+    if (randomize == 1)
+    {
+        for (j = 0; j < d; j++)
+        {
+            U = MRG63k3a(); /* 63 bit U(0,1) random number */
+            for (i = 0; i < n; i++)
+            {
+                ij = j * n + i;
+                res[ij] = res[ij] + U;
+                if (res[ij] > 1)
+                    res[ij] = res[ij] - 1.0;
+            }
+        }
+    }
+    free(aux);
 }
