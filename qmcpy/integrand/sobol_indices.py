8b7ca61495e6ef3fd1febf4f3e1d4e252fe4a79f
alegresor <agsorokin3@gmail.com>
2022-02-16 19:14:05 -0600

    patch sobol_indices

--- qmcpy/integrand/sobol_indices.py
+++ qmcpy/integrand/sobol_indices.py
@@ -215,7 +215,7 @@ class SobolIndices(Integrand):
             flags_closed = compute_flags[0,k,:]
             flags_total = compute_flags[1,k,:]
             flags_k = flags_closed|flags_total
-            if not flags_total.any(): continue
+            if not flags_k.any(): continue
             u_bool = self.indices_bool_mat[k]
             not_u_bool = self.not_indices_bool_mat[k]
             v[:,u_bool] = x[:,u_bool]
@@ -253,3 +253,5 @@ class SobolIndices(Integrand):
         total_flags = flags_comb[1]
         individual_flags[:,-1] = (closed_flags|total_flags).any(0)
         return individual_flags
+
+class SensitivityIndices(SobolIndices): pass
