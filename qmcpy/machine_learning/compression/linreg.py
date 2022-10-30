94ed0a45aff7276e64178dfa0643ebec5d95404f
Sou-Cheng Choi <schoi32@iit.edu>
2022-10-30 17:18:10 -0500

    Example 1

---
+++ qmcpy/machine_learning/compression/linreg.py
@@ -0,0 +1,84 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from ctypes import *
+from numpy.ctypeslib import ndpointer
+from sklearn.preprocessing import MinMaxScaler
+
+np.set_printoptions(precision=3)
+
+# Load the diabetes dataset
+mu, sigma = 0, 1
+Ndata, s = 1000000, 6
+X = np.random.default_rng().normal(mu, sigma, size=(Ndata, s))
+y = np.random.default_rng().uniform(mu, sigma, Ndata)
+n_samples = X.shape[0]
+
+min_max_scaler = MinMaxScaler()
+X_minmax = min_max_scaler.fit_transform(X)
+
+# Create equal weights except for the last ten
+sample_weight = np.ones(n_samples) * 20
+sample_weight[-10:] *= 30
+sample_weight = sample_weight / sample_weight.max()
+
+# The unweighted model
+regr = LinearRegression()
+mod = regr.fit(X_minmax, y)
+yfit = mod.predict(X_minmax)
+# print(f"{yfit = }")
+mse = mean_squared_error(y, yfit)
+print(f"{mse = }")
+print(f"{mod.coef_ = }")
+
+# The weighted model
+regr = LinearRegression()
+mod = regr.fit(X_minmax, y, sample_weight)
+yfit = mod.predict(X_minmax)
+mse = mean_squared_error(y, yfit, sample_weight=sample_weight)
+print(f"\nWeighted {mse = }")
+print(f"{sample_weight.shape = }")
+print(f"{mod.coef_ = }")
+
+# load QMC points
+m, nu = 10, 3
+Nqmc = 100
+outs = s
+qmc_points = np.loadtxt('sobol.dat')  # 4097 x 100
+qmc_points = qmc_points[0:Nqmc, 0:s]  # Nqmc x s
+
+# load c functions
+lib = cdll.LoadLibrary("../c_lib/c_lib.cpython-39-darwin.so")
+computeWeights = lib.computeWeights
+computeWeights.restype = ndpointer(dtype=c_double, shape=(1 + outs, Nqmc))
+
+# compute weights
+print(f"\n{nu = }, {m = }, {s = }, {Ndata = }, {Nqmc = }, {outs = }")
+weights = computeWeights(c_int(nu), c_int(m), c_int(s), c_int(Ndata), c_int(Nqmc), c_int(outs),
+                         c_void_p(X.ctypes.data), c_void_p(qmc_points.ctypes.data), c_void_p(y.ctypes.data))
+weights = np.transpose(weights)
+print(f"{weights.shape = }")
+
+# The compressed data model
+regr = LinearRegression()
+mod = regr.fit(weights[:, :-1], weights[:, -1])
+yfit = mod.predict(weights[:, :-1])
+mse = mean_squared_error(weights[:, -1], yfit)
+print(f"Compressed data {mse = }")
+print(f"{mod.coef_ = }")
+
+"""
+mse = 0.08328613078165403
+mod.coef_ = array([-0.001,  0.004, -0.002,  0.002,  0.001, -0.001])
+
+Weighted mse = 0.08329050285745367
+sample_weight.shape = (1000000,)
+mod.coef_ = array([-0.002,  0.004, -0.002,  0.002,  0.001, -0.001])
+
+nu = 3, m = 10, s = 6, Ndata = 1000000, Nqmc = 100, outs = 6
+weights.shape = (100, 7)
+Compressed data mse = 1.6540768918948703e-14
+mod.coef_ = array([-0.022,  0.677, -0.653,  0.492,  0.207,  0.339])
+"""
