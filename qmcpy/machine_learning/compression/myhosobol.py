0639cabe1b5efe305b5eb8c5ee09d130c4fccbb7
Sou-Cheng Choi <schoi32@iit.edu>
2023-01-21 15:07:36 -0600

    MyHOSobol with unit tests for d=1

--- qmcpy/machine_learning/compression/myhosobol.py
+++ qmcpy/machine_learning/compression/myhosobol.py
@@ -1,85 +1,190 @@
 import numpy as np
 
-def format_bin(A):
+# TODO move utility functions to util
+
+
+def dec2bin(a, numbits=1):
     """
-    It takes an integer and returns a string that represents the binary representation of that integer
+    Produces a binary representation of an integer a with at least numbits bits.
 
     Args:
-      A: The number to be converted to binary
+        a: an integer
+        numbits: an integer representing the minimum number of bits in the binary representation
+
+    Returns:
+        a string of '0' and '1' representing the binary representation of a with at least numbits bits
+
+    Raises:
+        ValueError: If a is not an integer
+
+    Reference:
+        https://www.mathworks.com/help/matlab/ref/dec2bin.html
+
+    Examples:
+    >>> dec2bin(23)
+    '10111'
+
+    >>> dec2bin(23, 8)
+    '00010111'
+
+    >>> dec2bin(23,1)
+    '10111'
+
+    >>> dec2bin(-1, 8)
+    '11111111'
+
+    >>> dec2bin(-16, 8)
+    '11110000'
+
+    >>> dec2bin(0)
+    '0'
+
+    >>> dec2bin(10.5)
+    '1010'
+
+    >>> dec2bin('1')
+    Traceback (most recent call last):
+    ...
+    ValueError: Unknown type of a, <class 'str'>
+
+    >>> dec2bin([1023, 122, 14])
+    ['1111111111', '0001111010', '0000001110']
+
+    >>> dec2bin([12, 20, 36])
+    ['001100', '010100', '100100']
+
+    >>> dec2bin([-1, -16], 8)
+    ['11111111', '11110000']
+    """
+    if type(a) in [list, np.ndarray]:
+        max_abs_a = max(np.abs(a))
+        numbits = max(numbits, len(dec2bin(max_abs_a)))
+        return [dec2bin(aa, numbits) for aa in a]
+
+    if np.issubdtype(type(a), np.floating):
+        a = int(np.floor(a))
+
+    if not np.issubdtype(type(a), np.integer):
+        raise ValueError(f"Unknown type of a, {type(a)}")
+
+    if a >= 0:
+        return format(a, '0{}b'.format(numbits))
+    else:
+        return format(2**numbits + a, '0{}b'.format(numbits))
+
+
+def bin_zfill(a):
+    """
+    It takes an integer and returns a string that represents the binary representation of that integer.
+
+    Args:
+      a: The number to be converted to binary
 
     Returns:
       The binary representation of the number a.
-    """
-    if type(A) in [np.uint8, np.int8]:
-        format_string = "{:08b}"
-    elif type(A) in [np.uint16, np.int16]:
-        format_string = "{:016b}"
-    elif type(A) in [int, np.uint32, np.int32]:
-        format_string = "{:032b}"
-    elif type(A) in [np.uint64, np.int64]:
-        format_string = "{:064b}"
-    else:
-        raise ValueError("Unknown type of A, {type(A)}")
-    return "0b"+format_string.format(A)
 
-def bin_len_max(A):
+    Examples:
+    >>> bin_zfill(23)
+    '0b00000000000000000000000000010111'
+
+    >>> bin_zfill(np.uint8(23))
+    '0b00010111'
+
+    >>> bin_zfill(np.uint16(23))
+    '0b0000000000010111'
+
+    >>> bin_zfill(np.int8(-1))
+    '0b11111111'
+
+    >>> bin_zfill(np.int8(-16))
+    '0b11110000'
+
+    >>> bin_zfill(np.half(10.5))
+    '0b0000000000001010'
+
+    >>> bin_zfill('1')
+    Traceback (most recent call last):
+    ...
+    ValueError: Unknown type of a, <class 'str'>
+
+    >>> bin_zfill(np.array([1023, 122, 14], dtype=np.uint16))
+    ['0b0000001111111111', '0b0000000001111010', '0b0000000000001110']
+
+    >>> bin_zfill(np.array([12, 20, 36], dtype=np.int8))
+    ['0b00001100', '0b00010100', '0b00100100']
+
+    >>> bin_zfill(np.array([-1, -16], dtype=np.int8))
+    ['0b11111111', '0b11110000']
     """
-    It returns the maximum number of bits needed to represent the largest number in the integer class of A.
+    if type(a) in [list, np.ndarray]:
+        return [bin_zfill(aa) for aa in a]
+
+    l = bin_len_max(a)
+    y = "0b" + dec2bin(a, l)
+    #print(f"{a = }, {type(a) = }, {l = }, {y = }")
+    return y
+
+def bin_len_max(a):
+    """
+    It returns the maximum number of bits needed to represent the largest number in the integer class of a.
 
     Args:
-      A: a signed or unsigned integer in Python or Numpy.
+      a: a signed or unsigned integer in Python or Numpy.
 
     Returns:
-      The maximum number of bits that can be used to represent the largest integer in the integer class of A.
+      The maximum number of bits that can be used to represent the largest integer in the integer class of a.
     """
-    if type(A) in [np.uint8, np.int8]:
+
+    if type(a) in [np.uint8, np.int8]:
         len_max = 8
-    elif type(A) in [np.uint16, np.int16]:
+    elif type(a) in [np.uint16, np.int16, np.half, np.float16]:
         len_max = 16
-    elif type(A) in [int, np.uint32, np.int32]:
+    elif type(a) in [int, np.uint32, np.int32, np.single]:
         len_max = 32
-    elif type(A) in [np.uint64, np.int64]:
+    elif type(a) in [np.uint64, np.int64, float, np.double]:
         len_max = 64
     else:
-        raise ValueError("Unknown type of A, {type(A)}")
+        raise ValueError(f"Unknown type of a, {type(a)}")
     return len_max
 
 
-def is_signed(A):
+def is_signed(a):
     """
-    If the type of A is an unsigned integer, return False. If the type of A is a signed integer,
+    If the type of a is an unsigned integer, return False. If the type of a is a signed integer,
     return True. Otherwise, raise an error if the type of integer is not known.
 
     Args:
-      A: a signed or unsigned integer in Python or Numpy.
+      a: a signed or unsigned integer in Python or Numpy.
 
     Returns:
       The function is_signed() returns a boolean value.
     """
-    if type(A) in [np.uint8, np.uint16, np.uint32, np.uint64]:
+    if type(a) in [np.uint8, np.uint16, np.uint32, np.uint64]:
         return False
-    elif type(A) in [int, np.int8, np.int16, np.int32, np.int64]:
+    elif type(a) in [int, np.int8, np.int16, np.int32, np.int64]:
         return True
     else:
-        raise ValueError("Unknown type of A, {type(A)}")
+        raise ValueError("Unknown type of a, {type(a)}")
 
 
-def bitget(A, BIT):
+def bitget(a, bit):
     """
-    Returns the value of the bit at position BIT in A.
+    Returns the value of the bit at position bit in a.
 
     Args:
-        A: a signed or unsigned integer.
-        BIT: an integer between 1 (least significant bit) and the number of bits in the integer class of A. Or a list of integers,
-             with each element being an integer between 1 and the number of bits in the integer class of A.
+        a: a signed or unsigned integer.
+        bit: an integer between 1 (least significant bit) and the number of bits in the integer class of a. Or a list of integers,
+             with each element being an integer between 1 and the number of bits in the integer class of a.
 
     Returns:
-        an integer or a list of integers representing the value of the bit at position BIT in A.
+        an integer or a list of integers representing the value of the bit at position bit in a.
 
     Raises:
-        TypeError:  If A is not an integer.
-        ValueError: If BIT is not between 1 and the number of bits in the integer class of A.
+        TypeError:  If a is not an integer.
+        ValueError: If bit is not between 1 and the number of bits in the integer class of a.
 
+    Reference:
+        https://www.mathworks.com/help/matlab/ref/bitget.html
 
     Examples:
     >>> bit = list(range(8, 0, -1))
@@ -92,114 +197,153 @@ def bitget(A, BIT):
     >>> bitget(np.uint8(127), bit)
     [1, 1, 1, 1, 1, 1, 1, 1]
     """
-    if type(BIT) == list:
-        return [bitget(A, b) for b in BIT]
+    if type(bit) == list:
+        return [bitget(a, b) for b in bit]
 
-    l = bin_len_max(A)
-    if type(BIT) == int:
-        if type(A) in [int, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64]:
-            if (BIT > 0) and (BIT <= len(bin(A)[2:])):
-                return (A >> (BIT - 1)) & 1
-            elif BIT < l:
+    l = bin_len_max(a)
+    if type(bit) == int:
+        if type(a) in [int, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64]:
+            if (bit > 0) and (bit <= len(bin(a)[2:])):
+                return (a >> (bit - 1)) & 1
+            elif bit < l:
                 return 0
-            elif BIT == l:
-                return 0 if is_signed(A) else 1  # TODO debug
+            elif bit == l:
+                return 0 if is_signed(a) else 1  # TODO debug
             else:
-                raise ValueError("BIT must be between 1 and the number of bits in the integer class of A.")
+                raise ValueError("bit must be between 1 and the number of bits in the integer class of a.")
 
-    elif type(A) == list:
+    elif type(a) in [list, np.ndarray]:
         print("Contact QMCPy team for implementation of this case.")
     else:
-        raise TypeError("A must be an integer or a list of integers.")
+        raise TypeError("a must be an integer or a list of integers.")
 
 
-def bitset(A, BIT, V=1):
+def bitset(a, bit, v=1):
     """
-    Sets bit position BIT in A to 1 or 0.
+    Sets bit position bit in a to 1 or 0.
 
     Args:
-        A: a signed or unsigned integer or an array of integers.
-        BIT: an integer between 1 (least significant bit) and the number of bits in the integer class of A. If A is a double array, then all elements
-        must be non-negative integers less than or equal to intmax('uint64'), and BIT must be between 1 and 64.
-        V: Zero values of V sets the bit to 0 (off), and non-zero values of V sets the bit to 1 (on).
+        a: a signed or unsigned integer or an array of integers.
+            If a is a double array, then MATLAB® treats A as an unsigned 64-bit integer. TODO
+        bit: an integer between 1 (least significant bit) and the number of bits in the integer class of a. If a is a double array, then all elements
+        must be non-negative integers less than or equal to intmax('uint64'), and bit must be between 1 and 64.
+        v: Zero values of v sets the bit to 0 (off), and non-zero values of v sets the bit to 1 (on).
 
     Returns:
-        an integer or a list of integers representing A with bit position BIT set to 1.
+        an integer or a list of integers representing a with bit position bit set to 1.
 
     Raises:
-        TypeError: If A is not an integer or a list of integers.
-        ValueError: If BIT is not between 1 and the number of bits in the integer class of A or BIT is not between 1 and 64
-            if A is a list of integers.
+        TypeError: If a is not an integer or a list of integers.
+        ValueError: If bit is not between 1 and the number of bits in the integer class of a or bit is not between 1 and 64
+            if a is a list of integers.
+
+    Reference:
+        https://www.mathworks.com/help/matlab/ref/bitset.html
 
     Examples:
     >>> bitset(np.uint8(9), 5, 1)
     25
-    """
-    if type(BIT) == list:
-        for b in BIT:
-            A = bitset(A, b, V)
-            return A
 
-    l = bin_len_max(A)
-    if type(A) in [int, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64]:
-        if (BIT > 0) and (BIT <= len(format_bin(A)[2:])):
-            return A | (V << (BIT - 1))
-        elif BIT < l:
-            return 0
-        elif BIT == l:
-            return 0 if is_signed(A) else 1  # TODO debug
+    >>> bitset(4, [4, 5, 6])
+    [12, 20, 36]
+
+    >>> bitset(np.int8(75), 5)
+    91
+    """
+    if type(bit) == list:
+        return [bitset(a, b, v) for b in bit]
+
+    #l = bin_len_max(a)
+    if type(a) in [float, int, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64]:
+        if (bit > 0) and (bit <= len(bin_zfill(a)[2:])):
+            return a | (v << (bit - 1))
         else:
-            raise ValueError(f"BIT must be between 1 and the number of bits {l} in the integer class of A.")
-    elif type(A) == list:
+            raise ValueError(f"bit must be between 1 and the number of bits {l} in the integer class of a.")
+    elif type(a) in [list, np.ndarray]:
         print("Contact QMCPy team for implementation of this case.")
         """
-        if BIT > 0 and BIT <= 64:
-            if all(isinstance(i, int) and i >= 0 and i <= int(2**64-1) for i in A):
-                return [i | (1 << (BIT-1)) for i in A]
+        if bit > 0 and bit <= 64:
+            if all(isinstance(i, int) and i >= 0 and i <= int(2**64-1) for i in a):
+                return [i | (1 << (bit-1)) for i in a]
             else:
-                raise ValueError("If A is a list, all elements must be non-negative integers less than or equal to intmax('uint64'), and BIT must be between 1 and 64.")
+                raise ValueError("If a is a list, all elements must be non-negative integers less than or equal to intmax('uint64'), and bit must be between 1 and 64.")
         else:
-            raise ValueError("BIT must be between 1 and 64.")
+            raise ValueError("bit must be between 1 and 64.")
         """
     else:
-        raise TypeError("A must be an integer or a list of integers.")
+        raise TypeError("a must be an integer or a list of integers.")
 
 def MyHOSobol(m, s, d=1):
-    # Higher order Sobol sequence
-    # Create a higher order Sobol sequence.
-    # 2^m number of points
-    # s dimension of final point set
-    # d interlacing factor
-    # X Output Sobol sequence
+    """
+    Higher-order Sobol sequence.
+
+    Args:
+        m: 2^m number of points
+        s: dimension of final point set
+        d: interlacing factor. Defaults to 1
+
+    Returns:
+        A matrix of higher order Sobol points, where the number of columns equals the dimension and the
+        number of rows equals the number of points.
+
+    Examples:
+    >>> MyHOSobol(0, 1)
+    array([[0.]])
+
+    >>> MyHOSobol(1, 1)
+    array([[0. ],
+           [0.5]])
+
+    >>> MyHOSobol(2, 1)
+    array([[0.  ],
+           [0.5 ],
+           [0.75],
+           [0.25]])
+
+    >>> MyHOSobol(3, 4)
+    array([[0.   , 0.   , 0.   , 0.   ],
+           [0.5  , 0.5  , 0.5  , 0.5  ],
+           [0.75 , 0.25 , 0.25 , 0.25 ],
+           [0.25 , 0.75 , 0.75 , 0.75 ],
+           [0.375, 0.375, 0.625, 0.875],
+           [0.875, 0.875, 0.125, 0.375],
+           [0.625, 0.125, 0.875, 0.625],
+           [0.125, 0.625, 0.375, 0.125]])
+
+    """
+
     z = np.loadtxt('sobol.dat')
     z = z[:2 ** m, :s * d]
     if d > 1:
-        print("Please contact the QMCPy team for this use case")
-        """
+        #print("Please contact the QMCPy team for this use case")
+
         N = pow(2, m) # Number of points;
         u = 52
-        depth = np.floor(u/d)
+        depth = int(np.floor(u/d))
         
-        # Create binary representation of digits;
-        
+        # Create binary representation of digits
         W = z * pow(2, np.int64(depth))
         Z = np.floor(np.transpose(W))
         Y = np.zeros((s, N))
         for j in range(s):
             for i in range(depth):
                 for k in range(d):
-                    Y[j,:] = bitset(Y[j,:], (depth*d+1) - k - (i-1)*d, bitget(Z[(j-1)*d+k,:], (depth+1) - i))
+                    a = Z[j*d+k, :]
+                    bit = depth - i
+                    v = bitget(a, bit)
+
+                    a = Y[j, :]
+                    bit = (depth*d+1) - (k+1) - i*d
+                    Y[j, :] = bitset(a, bit, v)
         Y = Y * pow(2, -depth*d)
 
-        X = np.transpose(Y) # X is matrix of higher order Sobol points,
-        # where the number of columns equals the dimension
-        # and the number of rows equals the number of points;
-        """
+        X = np.transpose(Y)
     else:
         X = z
 
     return X
 
 if __name__ == "__main__":
+
     import doctest
     doctest.testmod()
