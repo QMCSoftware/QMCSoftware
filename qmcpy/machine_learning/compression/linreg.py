fcce05b2c53aa4dbb516c00c76046cd8805d474e
Joshua Jay Herman <30265+zitterbewegung@users.noreply.github.com>
2023-01-23 12:55:55 -0600

    starting to add linear regression testing

--- qmcpy/machine_learning/compression/linreg.py
+++ qmcpy/machine_learning/compression/linreg.py
@@ -4,34 +4,35 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import numpy as np
-#from .c_lib import c_lib
+from .c_lib import c_lib
 from ctypes import *
 from numpy.ctypeslib import ndpointer
 from sklearn.preprocessing import MinMaxScaler
 import logging
 from myhosobol import MyHOSobol
 import math
+
 def approxmeanMXY(nu, m, x, y, d):
-    base = 2
-    s = x.shape[1]
-    MyHOSobol(m,s,d)
-    weights = MyHOSobol(m,s,d)
+	base = 2
+	s = x.shape[1]
+	MyHOSobol(m,s,d)
+	weights = MyHOSobol(m,s,d)
 
 def firstMissingPositive(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    bits = 0
-    for num in nums:
-        if 0 < num <= len(nums):
-            bits |= 1 << (num - 1)
+	"""
+	:type nums: List[int]
+	:rtype: int
+	"""
+	bits = 0
+	for num in nums:
+		if 0 < num <= len(nums):
+			bits |= 1 << (num - 1)
 
-    ret = 1
-    while bits & 1 != 0:
-        bits >>= 1
-        ret += 1
-    return ret
+	ret = 1
+	while bits & 1 != 0:
+		bits >>= 1
+		ret += 1
+	return ret
 
 
 
@@ -44,15 +45,15 @@ def main():
 	X = np.random.default_rng().normal(mu, sigma, size=(Ndata, s))
 	y = np.random.default_rng().uniform(mu, sigma, Ndata)
 	n_samples = X.shape[0]
-	
+
 	min_max_scaler = MinMaxScaler()
 	X_minmax = min_max_scaler.fit_transform(X)
-	
+
 	# Create equal weights except for the last ten
 	sample_weight = np.ones(n_samples) * 20
 	sample_weight[-10:] *= 30
 	sample_weight = sample_weight / sample_weight.max()
-	
+
 	# The unweighted model
 	print("Fitting the unweighted model")
 	regr = LinearRegression()
@@ -93,4 +94,4 @@ def main():
 	logging.info(f"{mod.coef_ = }")
 
 if __name__ == '__main__':
-    main()
+	main()
