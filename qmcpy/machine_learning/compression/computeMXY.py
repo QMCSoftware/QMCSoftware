2b2ae403bc8aa0e69127fca84f5f1752805285c1
Sou-Cheng Choi <schoi32@iit.edu>
2023-02-12 11:52:44 -0600

    clean up

--- qmcpy/machine_learning/compression/computeMXY.py
+++ qmcpy/machine_learning/compression/computeMXY.py
@@ -17,34 +17,25 @@ def computeMXY(nu, m, base, x, z, y):
     outs = 1
     s = x.shape[1]
     N = y.shape[0]
+
     # load c functions
     lib = cdll.LoadLibrary("../c_lib/c_lib.cpython-39-darwin.so")
-    computeWeights = lib.computeWeights
 
-    #TODO fill in call to c function computeWeights
+    # call to c function computeWeights
     computeWeights = lib.computeWeights
-    computeWeights.restype=ndpointer(dtype=c_double,shape=(1+outs, Nqmc))
+    computeWeights.restype = ndpointer(dtype=c_double,shape=(1+outs, Nqmc))
 
-    
     # compute weights
-    # We need to create a function using computemxy 
-  
-    
-    weights = computeWeights(c_int(nu),                              
-                         c_int(m),                                 
-                         c_int(s),                                  
-                         c_int(N),                                  
-                         c_int(Nqmc),                               
-                         c_int(base),                               
-                         c_void_p(x.ctypes.data),         
-        	             c_void_p(z.ctypes.data),
-                         c_void_p(y.ctypes.data))            
-    
+    weights = computeWeights(c_int(nu),
+                             c_int(m),
+                             c_int(s),
+                             c_int(N),
+                             c_int(Nqmc),
+                             c_int(base),
+                             c_void_p(x.ctypes.data),
+                             c_void_p(z.ctypes.data),
+                             c_void_p(y.ctypes.data))
 
     weights = np.transpose(weights)
-    # Return data mxArray* ret=mxCreateDoubleMatrix(Nqmc,2,mxREAL); /* return vector of weights */
-    #TODO remove the following line
-    #weights = np.loadtxt("./test_data/reg_weights.csv", delimiter=",")
-    #print(weights)
-    #np.savetxt('test.out', weights, delimiter=',')
+
     return weights
