56fec1477102c164394456c895f72101cfd913bb
Sou-Cheng Choi <schoi32@iit.edu>
2023-02-26 10:49:58 -0600

    Debugged px

--- qmcpy/machine_learning/compression/computeMXY.py
+++ qmcpy/machine_learning/compression/computeMXY.py
@@ -2,12 +2,12 @@ from ctypes import *
 import numpy as np
 from approxmeanMXY import *
 from numpy.ctypeslib import ndpointer
-
+import copy
 def computeMXY(nu, m, base, x, z, y):
     """
     >>> x = np.loadtxt("./test_data/reg_x.csv", delimiter=',')
     >>> y = np.loadtxt("./test_data/reg_y.csv", delimiter=",")
-    >>> z = np.loadtxt("./test_data/reg_z.csv", delimiter=',')
+    >>> z = np.loadtxt("./test_data/reg_z.csv", delimiter=',').T
     >>> weights = computeMXY(nu=1, m=6, base=2, x=x, z=z, y=y)
     >>> weights_true = np.loadtxt("./test_data/reg_weights.csv",  delimiter=",")
     >>> np.allclose(weights, weights_true, atol=1e-7)
@@ -25,16 +25,19 @@ def computeMXY(nu, m, base, x, z, y):
     computeWeights = lib.computeWeights
     computeWeights.restype = ndpointer(dtype=c_double, shape=(1+outs, Nqmc))
 
+    # Transpose the input matrix to change its storage order to column-major
+    x_transpose = np.asfortranarray(x)
+
     # compute weights
     weights = computeWeights(c_int(nu),
                              c_int(m),
                              c_int(s),
                              c_int(N),
                              c_int(Nqmc),
-                             c_void_p(x.ctypes.data),
+                             c_void_p(x_transpose.ctypes.data),
                              c_void_p(z.ctypes.data),
                              c_void_p(y.ctypes.data))
 
-    weights = np.transpose(weights.copy())
+    weights = np.transpose(copy.deepcopy(weights))
 
     return weights
