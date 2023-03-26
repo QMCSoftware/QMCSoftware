87e65df44859262446dc535c0d9a39e6b43ff6ee
Fred Hickernell <hickernell@iit.edu>
2023-03-26 15:52:16 -0500

    tweaked notebook execution

--- qmcpy/util/stop_notebook.py
+++ qmcpy/util/stop_notebook.py
@@ -1,8 +1,8 @@
 def stop_notebook(query = "Type 'yes' to continue running notebook"):
     #This is a function to be able to stop a notebook when you run all cells
-    keep_running = input("Type 'yes' to continue long code")
+    keep_running = input(query)
     if keep_running.casefold() != 'yes':
         import sys
         import warnings
         warnings.filterwarnings("ignore")
-        sys.exit('Stopping execution')
+        sys.exit('Pausing notebook execution')
