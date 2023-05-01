923602382237e19af00b8e0d86e4c326ba79eb45
Joshua Jay Herman <zitterbewegung@gmail.com>
2023-05-01 17:26:24 -0500

    Starting to use compression subclass

--- qmcpy/machine_learning/compression/compressor.py
+++ qmcpy/machine_learning/compression/compressor.py
@@ -1,68 +1,70 @@
 import logging
-from dataclasses import dataclass
-from myhosobol import MyHOSobol
-from typing import Any
-from approxmeanMXY import approxmeanMXY
 import numpy as np
+from myhosobol import MyHOSobol
+from approxmeanMXY import approxmeanMXY
+from compress import Compress
 
-@dataclass
-class digital_net_compression:
+class digital_net_compression(Compress):
 
-	'''
-	Args:
-	Computes the weights W_X,Y and W_X.
-	nu ... \nu in the paper
-	m ... \ell in the paper
-	s ... dimension of data
-	N ... number of datapoints
-	Nqmc ... number of qmc points
-	outs ... output dimension of the neural network (weights W_X,Y are vector valued if outs>1)
-	px ... pointer to datapoints array
-	pz ... pointer to qmc points array
-	py ... pointer to y values array
+	"""
+	>>> import compressor
+	>>> test = compressor.digital_net_compression(nu = 1, m=6, input_file="./test_data/reg_x.csv",label_file="./test_data/reg_y.csv" ,d=1)
+	>>> test.compute_weights()
+	Computation complete
+	"""
 
-	Output is a pointer to a vector which contains the weights W_X (Nqmc entries), and then the dimensions of W_X,Y (Nqmc x outs entries) in the same order as the qmc points.
-
-
-	Output is a pointer to a vector which contains the weights W_X (Nqmc entries),
-	and then the dimensions of W_X,Y (Nqmc x outs entries)  in the same order as the qmc points.
-	'''
-	nu: int
-	m: int
-	input_file: str
-	label_file: str
-	d: int
-	# def __init__(self, nu , m, s, N, Ndata, Nqmc, output_dimentsion, dataset):
-
-	#		self.m = m #ell in the paper
-	#		self.nu = nu
-	#		self.Ndata = Ndata
-	#		self.s = s
-	#		self.Nqmc = Nqmc	#		self.dataset = dataset
-	#		self.labels  = labels
-	#		self.sobol   = sobol
-	#		self.outputfile = outputfile
-	#		# load c functions
+	def __init__(self,
+				 nu: int,
+				 m: int,
+				 input_file: str,
+				 label_file: str,
+				 d: int,):
+				 #weights: str,
+				 #z: str):
+		self.nu = nu
+		self.m  = m
+		self.input_file = input_file
+		self.lable_file = label_file
+		self.d = d
+		#weights = weights
+		#z = z
 
 	def load_inputs(self):
-		return np.loadtxt(self.input_file, delimiter=',')
-		#logging.debug(self.dataset)
-		#return self.dataset
+		return np.loadtxt(self.input_file, delimiter=",")
+		# logging.debug(self.dataset)
+		# return self.dataset
 
 	def load_labels(self):
-		return np.loadtxt(self.label_file, delimiter=',')
-		#return self.labels
+		return np.loadtxt(self.label_file, delimiter=",")
+		# return self.labels
 
-	def get_sobol(self ):
-		'''Eventually we will replace this with the internal sobol library'''
-		#z = MyHOSobol(self.m, self.s, self.d)
-		#return self.sobol
+	def get_sobol(self):
+		"""Eventually we will replace this with the internal sobol library"""
+		# z = MyHOSobol(self.m, self.s, self.d)
+		# return self.sobol
 		pass
 
 	def compute_weights(self):
-		#x,  z , y = self.get_dataset()
-		#return computeMXY(self.nu, self.m, self.base, input, self.get_sobol(), self.labels)
-		weights, z = approxmeanMXY(self.nu,self.m,self.load_inputs(), self.load_labels(), self.d) 
-		np.savetxt('weights_computed.npy', weights)
-		np.savetxt('linear_regression_compresed.npy', z)
-		print('Computation complete')
+		#logging.debug(self.load_inputs())
+		#logging.debug(self.load_labels())
+
+		weights, z = approxmeanMXY(
+			self.nu, self.m, self.load_inputs(), self.load_labels(), self.d
+		)
+		np.savetxt(
+			"weights_computed.npy", self.weights
+		)  # TODO: Relative path with date and time, don't save into current pip install, make it not overwrite existing files.
+		np.savetxt(
+			"linear_regression_compresed.npy", self.z
+		)  # TODO: Relative path with date and time, don't save into current pip install, make it not overwrite existing files.
+		print("Computation complete")
+
+
+if __name__ == "__main__":
+	import logging
+
+	logging.basicConfig()
+	logging.getLogger().setLevel(logging.DEBUG)
+	import doctest
+
+	doctest.testmod()
