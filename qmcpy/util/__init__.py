60782f383bcf80db1d430858cef6765e93c029b7
aaditj1962161 <aaditdjain@gmail.com>
2023-07-04 20:13:57 +0530

    Setting bounds manually for discrete distribution

--- qmcpy/util/__init__.py
+++ qmcpy/util/__init__.py
@@ -2,3 +2,4 @@ from .exceptions_warnings import *
 from .abstraction_functions import _univ_repr
 from .math_functions import _tol_fun
 from .latnetbuilder_linker import latnetbuilder_linker
+from .plot_functions import plot_proj
