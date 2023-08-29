d1f9db017183fdc1521146ee0274dbf68354b56b
aaditj1962161 <aaditdjain@gmail.com>
2023-08-28 18:11:01 -0700

    Stopping criterions as a superclass

--- qmcpy/stopping_criterion/cub_mc_clt_vec.py
+++ qmcpy/stopping_criterion/cub_mc_clt_vec.py
@@ -11,7 +11,7 @@ from scipy.stats import norm
 from time import time
 import warnings
 
-class CubMCCLTVec(CubMCCLT):
+class CubMCCLTVec(StoppingCriterion):
     def __init__(self, integrand, abs_tol=1e-2, rel_tol=0., n_init=1024., n_max=1e10,
         inflate=1.2, alpha=0.01, control_variates=[], control_variate_means=[],
         error_fun = lambda sv,abs_tol,rel_tol: maximum(abs_tol,abs(sv)*rel_tol)):
@@ -29,9 +29,25 @@ class CubMCCLTVec(CubMCCLT):
             control_variate_means (list): list of means for each control variate
         """
 
-        super(CubMCCLTVec,self).__init__(integrand, abs_tol=1e-2, rel_tol=0., n_init=1024., n_max=1e10,
-        inflate=1.2, alpha=0.01, control_variates=[], control_variate_means=[],
-        error_fun = lambda sv,abs_tol,rel_tol: maximum(abs_tol,abs(sv)*rel_tol))
+        self.parameters = ['abs_tol','rel_tol','n_init','n_max','inflate','alpha']
+        # Set Attributes
+        self.abs_tol = abs_tol
+        self.rel_tol = rel_tol
+        self.n_init = n_init
+        self.n_max = n_max
+        self.alpha = alpha
+        self.inflate = inflate
+        # QMCPy Objs
+        self.integrand = integrand
+        self.true_measure = self.integrand.true_measure
+        self.discrete_distrib = self.integrand.discrete_distrib
+        self.cv = control_variates
+        self.cv_mu = control_variate_means
+        # Verify Compliant Construction
+        allowed_levels = ['single','fixed-multi']
+        allowed_distribs = [IID]
+        allow_vectorized_integrals = True
+        super(CubMCCLTVec,self).__init__(allowed_levels, allowed_distribs, allow_vectorized_integrals)
 
     def integrate(self):
         """
@@ -51,16 +67,49 @@ class CubMCCLTVec(CubMCCLT):
             print("upper bound = " + str(conf_int_hbound))
             return conf_int_lbound,conf_int_hbound
         """
-        super(CubMCCLTVec,self).integrate()
+         # Construct AccumulateData Object to House Integration data
+        self.data = MeanVarData(self, self.integrand, self.true_measure, self.discrete_distrib, 
+            self.n_init, self.cv, self.cv_mu)  # house integration data
+        t_start = time()
+        # Pilot Sample
+        self.data.update_data()
+        # use cost of function values to decide how to allocate
+        temp_a = self.data.t_eval ** 0.5
+        temp_b = (temp_a * self.data.sighat).sum()
+        # samples for computation of the mean
+        # n_mu_temp := n such that confidence intervals width and conficence will be satisfied
+        tol_up = max(self.abs_tol, abs(self.data.solution) * self.rel_tol)
+        z_star = -norm.ppf(self.alpha / 2.)
+        n_mu_temp = ceil(temp_b * (self.data.sighat / temp_a) * (z_star * self.inflate / tol_up)**2)
+        # n_mu := n_mu_temp adjusted for previous n
+        self.data.n_mu = maximum(self.data.n, n_mu_temp)
+        self.data.n += self.data.n_mu.astype(int)
+        if self.data.n_total + self.data.n.sum() > self.n_max:
+            # cannot generate this many new samples
+            warning_s = """
+            Alread generated %d samples.
+            Trying to generate %d new samples, which would exceed n_max = %d.
+            The number of new samples will be decrease proportionally for each integrand.
+            Note that error tolerances may no longer be satisfied.""" \
+            % (int(self.data.n_total), int(self.data.n.sum()), int(self.n_max))
+            warnings.warn(warning_s, MaxSamplesWarning)
+            # decrease n proportionally for each integrand
+            n_decease = self.data.n_total + self.data.n.sum() - self.n_max
+            dec_prop = n_decease / self.data.n.sum()
+            self.data.n = floor(self.data.n - self.data.n * dec_prop)
+        # Final Sample
+        self.data.update_data()
+        # CLT Confidence Interval
         print("mean = " + str(self.data.muhat))
         print("std = " + str(self.data.sighat))
         print("samples = " + str(self.data.n_total))
-        margin_error = sqrt(self.inflate) * -norm.ppf(self.alpha / 2) * (self.data.sighat / (sqrt(self.data.n_total)))
+        margin_error = sqrt(self.inflate) * z_star * (self.data.sighat / (sqrt(self.data.n_total)))
         print("margin of error = " + str(margin_error))
         conf_int_lbound = self.data.muhat - margin_error
         print("lower bound = " + str(conf_int_lbound))
         conf_int_hbound = self.data.muhat + margin_error
         print("upper bound = " + str(conf_int_hbound))
+        self.data.time_integrate = time() - t_start
         return conf_int_lbound,conf_int_hbound
 
 
