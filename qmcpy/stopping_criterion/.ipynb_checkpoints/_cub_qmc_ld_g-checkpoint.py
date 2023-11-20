1fbdfc653b32dde490ab224d369c75ee1e1c8a51
ggraoigr <ggraoigrasa@hawk.iit.edu>
2023-11-19 20:03:16 -0600

    update density estimation

--- qmcpy/stopping_criterion/.ipynb_checkpoints/_cub_qmc_ld_g-checkpoint.py
+++ qmcpy/stopping_criterion/.ipynb_checkpoints/_cub_qmc_ld_g-checkpoint.py
@@ -175,12 +175,17 @@ class _CubQMCLDG(StoppingCriterion):
         prob=len(values_cont)/len(g) #probability of being continous
         kde=sc.stats.gaussian_kde(values_cont.T)
         z=linspace(a,b,100)
+        hist=zeros(len(z))
+        for val, count in zip(repeated_values, counts):
+            index = (abs(z - val)).argmin() # Find the index in 'z' that is closest to 'val'
+            hist[index] = count # Assign the probability 'count' to the appropriate position in 'hist'
             
+
         pdf_est=kde.evaluate(z.T)*prob
         fig, ax1 = plt.subplots()
         color = 'tab:red'
-        ax1.set_xlabel('x')
-        ax1.set_ylabel('Density', color=color)
+        ax1.set_xlabel('payoff')
+        ax1.set_ylabel('density', color=color)
         ax1.plot(z, pdf_est, color=color)
         ax1.tick_params(axis='y', labelcolor=color)
         
