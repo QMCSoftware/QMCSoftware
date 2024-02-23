9f45b41f68337423a441d7ae2d8c8d00eba3f992
CDHJ2000 <cdhjr2000@gmail.com>
2024-02-23 11:17:04 -0600

    Discrepancy

--- qmcpy/kernels/discrepancy.py
+++ qmcpy/kernels/discrepancy.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 from copy import copy
 
-def discrepancy(method, x):
+def discrepancy(method, x, weight = 1):
     n, d = x.shape                          #Finds the number of samples and the dimensions for our x_i's
     X_expanded = np.zeros((n,n,d)) + x      #Copies x into a 3d matrix of size n by n by d.
     Y = np.resize(x, (n, 1, d))             #reshapes x so that we can iteratively find the value of the kernels
@@ -12,23 +12,23 @@ def discrepancy(method, x):
         #returns the discrepancy
         return np.sqrt(double_integral - (2*np.mean(single_integral)) + np.mean(np.mean(kernel)))
     else:
-        if method == "S" or method.lower() == "star":           #Star
+        if method.lower() == "s" or method.lower() == "star":           #Star
             double_integral = (4/3)**d
             single_integral = ((3-x**2)/2).prod(axis=1)
             kernel = (2 - np.maximum(X_expanded, Y)).prod(axis=2)
-        elif method == "L2" or method.lower() == "l2star":        #L2star
+        elif method.lower() == "l2" or method.lower() == "l2star":        #L2star
             double_integral = (1/3)**d
             single_integral = ((1-x**2)/2).prod(axis=1)
             kernel = (1 - np.maximum(X_expanded, Y)).prod(axis=2)
-        elif method == "C" or method.lower() == "centered":         #Centered
+        elif method.lower() == "c" or method.lower() == "centered":         #Centered
             double_integral = (13/12)**d
             single_integral = (1 + (.5*abs(x - .5)) - (.5*((x -.5)**2))).prod(axis=1)
             kernel = (1 + (.5*abs(X_expanded - .5)) + (.5*abs(Y - .5)) - (.5*abs(X_expanded - Y))).prod(axis=2)
-        elif method == "Sy" or method.lower() == "symmetric":        #Symmetric
+        elif method.lower() == "sy" or method.lower() == "symmetric":        #Symmetric
             double_integral = (4/3)**d
             single_integral = (1 + 2*x - (2*(x**2))).prod(axis=1)
             kernel = (2 - (2*abs(X_expanded - Y))).prod(axis=2)
-        elif method == "WA" or method.lower() == "wrap around" or method.lower() == "wrap-around":        #Wrap around
+        elif method.lower() == "wa" or method.lower() == "wrap around" or method.lower() == "wrap-around":        #Wrap around
             double_integral = -(4/3)**d
             single_integral = 0
             kernel = (1.5 - (abs(X_expanded - Y)*(1 - abs(X_expanded - Y)))).prod(axis=2)
