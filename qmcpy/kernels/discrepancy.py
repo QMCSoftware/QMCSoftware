4b40ff61d10e053c0f2c66293f61bb845c0199f3
CDHJ2000 <cdhjr2000@gmail.com>
2024-05-14 10:55:32 -0500

    Discrepancy

--- qmcpy/kernels/discrepancy.py
+++ qmcpy/kernels/discrepancy.py
@@ -4,7 +4,64 @@ from copy import copy
 from inspect import signature
 import math
 
-def discrepancy(method, x, weight = 1, limiter = 2**22):
+def discrepancy(method, x, weight = 1):
+    n, d = x.shape                              #Finds the number of samples and the dimensions for our x_i's
+    weight = weight * np.ones(d)                #if weight is a scalar, it gets turned into an array.
+                                                #if weight is an array, it would still be an array since
+                                                #np.ones(d) would be identified as an identity.
+    #find a way to generate the weight, and generate the scalar into an array
+    #For L2 star, the weighted kernel is 
+    #(double integral)\prod_{j=1}^d (1 + (w_j)/3)
+    #(single integral)\prod_{j=1}^d (1 + (w_j)(1-x^2)/2)
+    #(kernel) \prod_{j=1}^d (1 + w_j(1 - max(x_j, t_j)))
+
+    if callable(method):      #discrepancy function given by the user
+        sig = signature(method)
+        params = sig.parameters
+        if len(params) == 1:
+            #The function given by the discrepancy function must return the double integral, second integral, and kernel in this order
+            double_integral, single_integral, kernel = method(x)
+            #returns the discrepancy
+        elif len(params) == 2:
+            #The weighted discrepancy given by the user
+            double_integral, single_integral, kernel = method(x, weight)
+        return np.sqrt(double_integral - (2*np.mean(single_integral)) + np.mean(np.mean(kernel)))
+    else:
+        #X_expanded = np.zeros((n,n,d)) + x      #Copies x into a 3d matrix of size n by n by d.
+        X_expanded = np.resize(x, (1, n, d))
+        Y = np.resize(x, (n, 1, d))             #reshapes x so that we can iteratively find the value of the kernels
+        if method.lower() == "l2" or method.lower() == "l2star":           #Star
+            double_integral = (1 + (weight/3)).prod(axis=0)
+            single_integral = ((1 + (weight*(1 - x**2)/2))).prod(axis=1)
+            kernel = (1 + weight*(1 - np.maximum(X_expanded, Y))).prod(axis=2)
+        #elif method.lower() == "s" or method.lower() == "star":        #L2star
+        #    double_integral = (1/3)**d
+        #    single_integral = ((1-x**2)/2).prod(axis=1)
+        #    kernel = (1 - np.maximum(X_expanded, Y)).prod(axis=2)
+        elif method.lower() == "c" or method.lower() == "centered" or method.lower() == 'cd':         #Centered
+            double_integral = (13/12)**d
+            single_integral = (1 + (.5*abs(x - .5)) - (.5*((x -.5)**2))).prod(axis=1)
+            kernel = (1 + (.5*abs(X_expanded - .5)) + (.5*abs(Y - .5)) - (.5*abs(X_expanded - Y))).prod(axis=2)
+        elif method.lower() == "sy" or method.lower() == "symmetric":        #Symmetric
+            double_integral = (4/3)**d
+            single_integral = (1 + 2*x - (2*(x**2))).prod(axis=1)
+            kernel = (2 - (2*abs(X_expanded - Y))).prod(axis=2)
+        elif method.lower() == "wa" or method.lower() == "wrap around" or method.lower() == "wrap-around" or method.lower() == 'wd':        #Wrap around
+            double_integral = -(4/3)**d
+            single_integral = 0
+            kernel = (1.5 - (abs(X_expanded - Y)*(1 - abs(X_expanded - Y)))).prod(axis=2)
+            #double_integral = -(4/3)**d
+            #single_integral
+        elif method.lower() == "m" or method.lower() == "mixture" or method.lower() == 'md':        #Wrap around
+            double_integral = (19/12)**d
+            single_integral = ((5/3) - (.25*abs(x - .5)) - (.25*((x -.5)**2))).prod(axis=1)
+            kernel = (1.875 - (.25*abs(X_expanded - .5)) - (.25*abs(Y - .5)) - (.75*abs(X_expanded - Y)) + (.5*((X_expanded - Y)**2))).prod(axis=2)
+        else:
+            return False
+    #returns the discrepancy
+    return np.sqrt(double_integral - (2*np.mean(single_integral)) + np.mean(np.mean(kernel)))
+
+def discrepancy2(method, x, weight = 1, limiter = 2**22):
     n, d = x.shape                              #Finds the number of samples and the dimensions for our x_i's
     weight = weight * np.ones(d)                #if weight is a scalar, it gets turned into an array.
                                                 #if weight is an array, it would still be an array since
@@ -131,4 +188,3 @@ def discrepancy(method, x, weight = 1, limiter = 2**22):
             return False
     #returns the discrepancy
     return np.sqrt(double_integral - (2*np.mean(single_integral)) + np.mean(np.mean(kernel)))
-
