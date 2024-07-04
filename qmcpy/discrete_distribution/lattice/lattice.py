4782b357166082321d917214329461eabcad677f
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2024-07-04 21:13:28 +0000

    Fix doc warnings

--- qmcpy/discrete_distribution/lattice/lattice.py
+++ qmcpy/discrete_distribution/lattice/lattice.py
@@ -116,25 +116,25 @@ class Lattice(LD):
         """
         Args:
             dimension (int or ndarray): dimension of the generator.
-                If an int is passed in, use sequence dimensions [0,...,dimensions-1].
-                If a ndarray is passed in, use these dimension indices in the sequence.
+                If an int is passed in, use sequence dimensions [0,...,`dimension`-1].
+                If an ndarray is passed in, use these dimension indices in the sequence.
             randomize (bool): If True, apply shift to generated samples.
                 Note: Non-randomized lattice sequence includes the origin.
-            order (str): 'linear', 'natural', or 'mps' ordering.
+            order (str): `'linear'`, `'natural'`, or `'mps'` ordering.
             seed (None or int or numpy.random.SeedSeq): seed the random number generator for reproducibility
             generating_vector (ndarray or str or int): Specify the generating matrix. There are a number of optional input types. 
-                1) A ndarray of integers. 
-                2) A string generating_vector with either: 
-                    i)  a relative path from https://github.com/QMCSoftware/LDData e.g. "LDData/main/lattice/mps.exod2_base2_m20_CKN.txt"  or 
-                    ii) a numpy file with format "name.d_max.m_max.npy" e.g. 'lattice_vec.3600.20.npy'.
-                3) An odd integer 1 < M < 27 which creates a random generating vector [1,v_1,v_2,...,v_{d_max}] where v_i is a random integer in {3,5,...,2*M-1} supporting up to 2^M points.
+                1. AN ndarray of integers. 
+                2. A string generating_vector with either: 
+                    i)  a relative path from https://github.com/QMCSoftware/LDData, e.g., `LDData/main/lattice/mps.exod2_base2_m20_CKN.txt`  or 
+                    ii) a numpy file with format `name.d_max.m_max.npy`, e.g., `lattice_vec.3600.20.npy`.
+                3. An odd integer :math:`1 < M < 27` which creates a random generating vector :math:`[1,v_1,v_2,...,v_{d_max}]` where :math:`v_i` is a random integer in :math:`{3,5,...,2*M-1}` supporting up to :math:`2^M` points.
             d_max (int): maximum dimension
-            m_max (int): 2^m_max is the max number of supported samples
+            m_max (int): :math:`2^{m_max}` is the max number of supported samples
             is_parallel (bool): Default to True to perform parallel computations, False serial
 
         Note:
-            d_max and m_max are required if generating_vector is a ndarray.
-            If generating_vector is an string (path), d_max and m_max can be taken from the file name if None
+            `d_max` and `m_max` are required if `generating_vector` is an ndarray.
+            If `generating_vector` is a string (path), `d_max` and `m_max` can be taken from the file name if None.
         """
         self.parameters = ['dvec','randomize','order']
         self.randomize = randomize
