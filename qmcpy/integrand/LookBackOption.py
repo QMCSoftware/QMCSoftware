5314f8dcf71ac04eef3d0350df07224d82adfbe0
YuhanDing <jinliang98@hotmail.com>
2024-08-06 19:45:03 -0500

    Update LookBackOption
    Add pricing options demo to the demos fold; Update LookBackOption

--- qmcpy/integrand/LookBackOption.py
+++ qmcpy/integrand/LookBackOption.py
@@ -7,10 +7,8 @@ from ._option import Option
 class LookBackOption(Option):
                           
     def __init__(self, sampler, volatility=0.5, start_price=30.0, interest_rate=0.0,
-                 drift=0, n=16, t_final=1, call_put='put', multilevel_dims=None,
-                 _dim_frac=0, observations=10):      
-        self.observations = observations
-        self.n = n # Number of samples
+                 t_final=1, call_put='put', multilevel_dims=None, 
+                 _dim_frac=0,decomp_type='PCA'):      
         
         # Call super class's constructor to define this class's attributes
         super(LookBackOption, self).__init__(sampler, volatility, start_price,
@@ -18,48 +16,34 @@ class LookBackOption(Option):
                                              call_put, multilevel_dims, _dim_frac)
         
         # Finish defining the rest of the class's attributes
-        self.t = np.linspace(0, self.t_final, self.observations)
-        self.true_measure = BrownianMotion(self.sampler, t_final)
-        
-    def g(self, t):
-        """See abstract method."""
-        s = self.start_price * np.exp((self.interest_rate
-                                       - self.volatility**2 / 2.0)
-                                       * self.true_measure.time_vec
-                                       + self.volatility * t)
-        for xx, yy in zip(*np.where(s < 0)):
-            s[xx, yy:] = 0
-        if self.call_put == 'call':
-            y_raw = s[:,-1] - np.min(s, axis=1)
-        else: # put
-            y_raw = np.max(s, axis=1) - s[:,-1]
-        y_adj = y_raw * np.exp(-self.interest_rate * self.t_final)
-        return y_adj
-
-    # def _get_discounted_payoffs(self):
-    #     payoffs = []
-    #     if self.call_put == 'call':
-    #         for i in range(self.n):
-    #             strike_price = min(self.stock_values[i,:])
-    #             payoff = self.stock_values[i,self.observations-1] - strike_price
-    #             payoffs.append(payoff)
-    #     else: # put
-    #         for i in range(self.n):
-    #             strike_price = max(self.stock_values[i,:])
-    #             payoff = strike_price - self.stock_values[i,self.observations-1]
-    #             payoffs.append(payoff)
-    #     return np.mean(payoffs)
+        self.true_measure = BrownianMotion(self.sampler, self.t_final, decomp_type=decomp_type)
     
-    # def get_discounted_payoffs(self):
-    #     """
-    #     Wrapper method for the private _get_discounted_payoffs method.
-    #     Although the user could just call the original method directly,
-    #     it's better to stick with Python convention and not call the
-    #     private method directly, but instead through a wrapper. The
-    #     reason for this is we still want to user to be able use this,
-    #     while also overriding the abstract method as well.
-    #     """
-    #     return self._get_discounted_payoffs()
+    def _get_discounted_payoffs(self,stock_path,dimension):
+        if self.call_put == 'call':
+           y_raw = stock_path[:,-1] - np.minimum(np.min(stock_path, axis=1),self.start_price)
+        else: # put
+            y_raw = np.maximum(np.max(stock_path, axis=1),self.start_price) - stock_path[:,-1]
+        y_adj = y_raw * np.exp(-self.interest_rate * self.t_final)
+        return y_adj 
+     
+    def g(self, t):
+        if self.parent:
+            raise ParameterError('''
+                Cannot evaluate an integrand with multilevel_dims directly,
+                instead spawn some children and evaluate those.''')
+        self.s_fine = self.start_price * np.exp((self.interest_rate
+                                              - self.volatility**2 / 2.0)
+                                              * self.true_measure.time_vec
+                                              + self.volatility * t)
+        for xx,yy in zip(*np.where(self.s_fine<0)): # if stock becomes <=0, 0 out rest of path
+            self.s_fine[xx,yy:] = 0
+        y = self._get_discounted_payoffs(self.s_fine, self.d)
+        if self.dim_fracs > 0:
+            s_coarse = self.s_fine[:, int(self.dim_fracs - 1):: int(self.dim_fracs)]
+            d_coarse = float(self.d) / self.dim_fracs
+            y_coarse = self._get_discounted_payoffs(s_coarse, d_coarse)
+            y -= y_coarse
+        return y
         
     def _spawn(self, level, sampler):            
         return LookBackOption(
