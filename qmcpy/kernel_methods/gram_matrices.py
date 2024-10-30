7d927046d1a927697fa2befd8a59a1824be57a5c
alegresor <agsorokin3@gmail.com>
2024-10-29 19:58:57 -0500

    start gram matrices

--- qmcpy/kernel_methods/gram_matrices.py
+++ qmcpy/kernel_methods/gram_matrices.py
@@ -2,14 +2,57 @@ from ..discrete_distribution import Lattice,DigitalNetB2
 from .kernels import KernelShiftInvar,KernelDigShiftInvar
 from .fast_transforms import fft_bro_1d_radix2,ifft_bro_1d_radix2,fwht_1d_radix2
 import numpy as np
-    
+
 class FastGramMatrix(object):
-    def __init__(self, dd_obj, kernel_obj, n, ft, ift):
-        assert dd_obj.d==kernel_obj.d 
-        assert dd_obj.replications==1
-        assert (n&(n-1))==0 # require n is 0 or a power of 2
+    def __init__(self, dd_obj, kernel_obj, n, ft, ift, noise):
+        self.dd_obj = dd_obj
+        self.kernel_obj = kernel_obj
+        self.n = n
         self.ft = ft 
         self.ift = ift
+        self.noise = noise
+        assert self.dd_obj.d==self.kernel_obj.d 
+        assert self.dd_obj.replications==1
+        assert (self.n&(self.n-1))==0 # require n is 0 or a power of 2
+        assert isinstance(noise,float) and noise>=0
+        self._x,self.x = self.sample(0,n)
+        k1 = self.kernel_obj(self._x,self._x[[0]])[:,0]
+        self.lam = np.sqrt(n)*self.ft(k1)
+        self.lam[0] = self.lam[0]+self.noise
+    def get_full_gram_matrix(self):
+        return self.kernel_obj(self._x,self._x)
+    def multiply_yt(self, yt):
+        ytogndim = yt.ndim
+        assert ytogndim<=2 
+        if ytogndim==1: yt = yt[:,None]
+        assert yt.ndim==2 and yt.shape[0]==self.n
+        sol = self.ift(yt.T*self.lam).real.T
+        return sol[:,0] if ytogndim==1 else sol
+    def multiply(self, y):
+        yogndim = y.ndim
+        assert yogndim<=2 
+        if yogndim==1: y = y[:,None]
+        assert y.ndim==2 and y.shape[0]==self.n
+        yt = self.ft(y.T).T
+        sol = self.multiply_yt(yt)
+        return sol[:,0] if yogndim==1 else sol
+    def __matmul__(self, y):
+        return self.multiply(y)
+    def solve_yt(self, yt):
+        ytogndim = yt.ndim
+        assert ytogndim<=2 
+        if ytogndim==1: yt = yt[:,None]
+        assert yt.ndim==2 and yt.shape[0]==self.n
+        sol = self.ift(yt.T/self.lam).real.T
+        return sol[:,0] if ytogndim==1 else sol
+    def solve(self, y):
+        yogndim = y.ndim
+        assert yogndim<=2 
+        if yogndim==1: y = y[:,None]
+        assert y.ndim==2 and y.shape[0]==self.n
+        yt = self.ft(y.T).T
+        sol = self.solve_yt(yt)
+        return sol[:,0] if yogndim==1 else sol
 
 class FastGramMatrixLattice(FastGramMatrix):
     """
@@ -17,15 +60,39 @@ class FastGramMatrixLattice(FastGramMatrix):
     
     >>> n = 2**3 
     >>> d = 3 
-    >>> lat_obj = Lattice(d,randomize="SHIFT",seed=7)
+    >>> lat_obj = Lattice(d,seed=7)
     >>> kernel_si = KernelShiftInvar(d)
     >>> gm = FastGramMatrixLattice(lat_obj,kernel_si,n)
+    >>> y = np.vstack([np.sin(2*np.pi*gm.x).prod(1),np.cos(2*np.pi*gm.x).prod(1)]).T
+    >>> y.shape
+    (8, 2)
+    >>> gmatfull = gm.get_full_gram_matrix()
+    >>> assert np.allclose(gm@y[:,0],gmatfull@y[:,0],atol=1e-12)
+    >>> assert np.allclose(gm@y,gmatfull@y,atol=1e-12)
+    >>> assert np.allclose(gm.solve(y[:,0]),np.linalg.solve(gmatfull,y[:,0]),atol=1e-12)
+    >>> assert np.allclose(gm.solve(y),np.linalg.solve(gmatfull,y),atol=1e-12)
+    >>> yt = gm.ft(y.T).T
+    >>> yt.shape 
+    (8, 2)
+    >>> assert np.allclose(gm.multiply_yt(yt[:,0]),gmatfull@y[:,0],atol=1e-12)
+    >>> assert np.allclose(gm.multiply_yt(yt),gmatfull@y,atol=1e-12)
+    >>> assert np.allclose(gm.solve_yt(yt[:,0]),np.linalg.solve(gmatfull,y[:,0]),atol=1e-12)
+    >>> assert np.allclose(gm.solve_yt(yt),np.linalg.solve(gmatfull,y),atol=1e-12)
     """
-    def __init__(self, lat_obj, kernel_si, n):
-        assert isinstance(lat_obj,Lattice) and lat_obj.randomize=="SHIFT"
+    def __init__(self, lat_obj, kernel_si, n, noise=0.):
+        """
+        Args:
+            lat_obj (Lattice): lattice discrete distribution with randomize='shift'
+            kernel_si (KernelShiftInvar): shift invariant kernel
+            n (int): power of two number of points
+            noise (float): nugget term 
+        """
+        assert isinstance(lat_obj,Lattice) and lat_obj.randomize=="SHIFT" and lat_obj.order=="NATURAL"
         assert isinstance(kernel_si,KernelShiftInvar)
-        super(FastGramMatrixLattice,self).__init__(lat_obj,kernel_si,n,fft_bro_1d_radix2,ifft_bro_1d_radix2)
-        self.x = lat_obj.gen_samples(n_min=0,n_max=n)
+        super(FastGramMatrixLattice,self).__init__(lat_obj,kernel_si,n,fft_bro_1d_radix2,ifft_bro_1d_radix2,noise)
+    def sample(self, n_min, n_max):
+        x = self.dd_obj.gen_samples(n_min=n_min,n_max=n_max)
+        return x,x
 
 class FastGramMatrixDigitalNetB2(FastGramMatrix):
     """
@@ -36,11 +103,37 @@ class FastGramMatrixDigitalNetB2(FastGramMatrix):
     >>> dnb2_obj = DigitalNetB2(d,randomize="LMS_DS",seed=7)
     >>> kernel_dsi = KernelDigShiftInvar(d)
     >>> gm = FastGramMatrixDigitalNetB2(dnb2_obj,kernel_dsi,n)
+    >>> y = np.vstack([np.sin(2*np.pi*gm.x).prod(1),np.cos(2*np.pi*gm.x).prod(1)]).T
+    >>> y.shape
+    (8, 2)
+    >>> gmatfull = gm.get_full_gram_matrix()
+    >>> assert np.allclose(gm@y[:,0],gmatfull@y[:,0],atol=1e-12)
+    >>> assert np.allclose(gm@y,gmatfull@y,atol=1e-12)
+    >>> assert np.allclose(gm.solve(y[:,0]),np.linalg.solve(gmatfull,y[:,0]),atol=1e-12)
+    >>> assert np.allclose(gm.solve(y),np.linalg.solve(gmatfull,y),atol=1e-12)
+    >>> yt = gm.ft(y.T).T
+    >>> yt.shape 
+    (8, 2)
+    >>> assert np.allclose(gm.multiply_yt(yt[:,0]),gmatfull@y[:,0],atol=1e-12)
+    >>> assert np.allclose(gm.multiply_yt(yt),gmatfull@y,atol=1e-12)
+    >>> assert np.allclose(gm.solve_yt(yt[:,0]),np.linalg.solve(gmatfull,y[:,0]),atol=1e-12)
+    >>> assert np.allclose(gm.solve_yt(yt),np.linalg.solve(gmatfull,y),atol=1e-12)
     """
-    def __init__(self, dnb2_obj, kernel_dsi, n):
-        assert isinstance(dnb2_obj,DigitalNetB2) and dnb2_obj.randomize=="LMS_DS"
+    def __init__(self, dnb2_obj, kernel_dsi, n, noise=0.):
+        """
+        Args:
+            lat_obj (Lattice): lattice discrete distribution with randomize='shift'
+            kernel_si (KernelShiftInvar): shift invariant kernel
+            n (int): power of two number of points
+            noise (float): nugget term 
+        """
+        assert isinstance(dnb2_obj,DigitalNetB2) and dnb2_obj.randomize=="LMS_DS" and dnb2_obj.graycode==False
         assert isinstance(kernel_dsi,KernelDigShiftInvar)
         kernel_dsi.set_t(dnb2_obj.t_lms)
-        super(FastGramMatrixDigitalNetB2,self).__init__(dnb2_obj,kernel_dsi,n,fwht_1d_radix2,fwht_1d_radix2)
-        self.x = dnb2_obj.gen_samples(n_min=0,n_max=n,return_binary=True)
+        super(FastGramMatrixDigitalNetB2,self).__init__(dnb2_obj,kernel_dsi,n,fwht_1d_radix2,fwht_1d_radix2,noise)
+    def sample(self, n_min, n_max):
+        xb = self.dd_obj.gen_samples(n_min=n_min,n_max=n_max,return_binary=True)
+        xf = xb*2**(-self.kernel_obj.t)
+        return xb,xf
+
     
