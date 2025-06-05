b5dbc779f3e6dc345f7bf202472d9c2ddec66150
fjhickernell <hickernell@iit.edu>
2025-06-04 19:18:33 -0500

    Fred M2 Mac Updates

--- qmcpy/discrete_distribution/kronecker.py
+++ qmcpy/discrete_distribution/kronecker.py
@@ -18,29 +18,7 @@ class Kronecker(LD):
         elif sum(delta) != 0:
             self.delta = delta
 
-###Start off with a code with Kronecker sequence with
-###P = {x_i = i \alpha + \delta mod 1} with \alpha and \delta \in [0,1)^d
 
-def kronecker(d, alpha = None, delta = None):
-    """
-    Args:
-        n (int): number of samples to generate.
-        d (int): number of dimensions for those desired sample points
-        alpha (array): a 1 by d dimensional array in accordance to the Kronecker sequence
-        as its multiplier.
-            If alpha is not chosen by user, it will generate an array at which
-            alpha is in [0,1)^d
-            If alpha is chosen by user, it will use that vector in accordance to
-            Kronecker sequence.
-        delta (array): a 1 by d dimensional array in accordance to the Kronecker sequence
-        as its shift.
-            If delta was not chosen by user, it will generate an array such that delta is
-            in [0,1)^d
-            If delta is chosen by user, it will use that vector in accordance to
-            Kronecker sequence.
-    Returns """
-
-
-def gen_samples(self, n):
+    def gen_samples(self, n):
         i = arange(n).reshape((n, 1))
         return(((i*self.alpha) + self.delta)%1)
