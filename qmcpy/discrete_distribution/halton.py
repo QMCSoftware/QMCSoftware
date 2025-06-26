2e4660eb8ac676d1d3e4b00ccc275bc6bd4aa3a3
JimmyNguyenUCI <130193339+JimmyNguyenUCI@users.noreply.github.com>
2025-06-26 16:36:33 -0500

    Generate discrepancy plots

--- qmcpy/discrete_distribution/halton.py
+++ qmcpy/discrete_distribution/halton.py
@@ -14,8 +14,6 @@ class Halton(LD):
     
     >>> halton = Halton(2,randomize="LMS_PERM",seed=7)
     >>> halton.gen_samples(4)
-    >>> halton = Halton(2,randomize="LMS_PERM",seed=7)
-    >>> halton.gen_samples(4)
     array([[0.2143238 , 0.1243237 ],
            [0.8861583 , 0.5235922 ],
            [0.49631029, 0.84796825],
