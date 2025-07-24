42eeac821882c954d66a0ce8f27bd425b86cb91f
alegresor <agsorokin3@gmail.com>
2025-07-24 14:40:32 +0900

    before torch kernels

--- qmcpy/kernel/multitask_kernel.py
+++ qmcpy/kernel/multitask_kernel.py
@@ -1,5 +1,6 @@
 from .abstract_kernel import AbstractKernel
-from ..util.transforms import tf_identity,tf_exp_eps,tf_exp_eps_inv
+from .common_kernels import KernelGaussian
+from ..util.transforms import tf_identity,tf_exp_eps,tf_exp_eps_inv,insert_batch_dims
 import numpy as np 
 
 
@@ -13,6 +14,128 @@ class KernelMultiTask(AbstractKernel):
     parameterized for $T$ tasks by a factor $\mathsf{F} \in \mathbb{R}^{T \times r}$ and a diagonal $\boldsymbol{v} \in \mathbb{R}^T$ so that 
     
     $$\left[K_{\mathrm{task}}(i,j)\right]_{i,j=1}^T = \mathsf{F} \mathsf{F}^T + \mathrm{diag}(\boldsymbol{v}).$$ 
+    
+    Examples:
+        >>> kmt = KernelMultiTask(KernelGaussian(d=2),num_tasks=3,diag=[1,2,3])
+        >>> x = np.random.rand(4,2) 
+        >>> task = np.arange(3) 
+        >>> kmt(x[0],x[0],task[1],task[1]).item()
+        3.0
+        >>> kmt(x[0],x[0],task,task)
+        array([2., 3., 4.])
+        >>> kmt(x[0],x[0],task[:,None],task[None,:]).shape
+        (3, 3)
+        >>> kmt(x,x,task[1],task[1])
+        array([3., 3., 3., 3.])
+        >>> kmt(x[:3],x[:3],task,task)
+        array([2., 3., 4.])
+        >>> kmt(x,x,task[:,None],task[:,None]).shape
+        (3, 4)
+        >>> v = kmt(x[:3,None,:],x[None,:3,:],task,task)
+        >>> v.shape
+        (3, 3)
+        >>> np.allclose(v,kmt.base_kernel(x[:3,None,:],x[None,:3,:])*kmt.taskmat[task,task])
+        True
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None],task[:,None,None]).shape
+        (3, 4, 4)
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None,None],task[None,:,None,None]).shape
+        (3, 3, 4, 4)
+
+        Batched inference
+
+        >>> kernel_base = KernelGaussian(d=10,shape_lengthscales=(5,1),shape_scale=(3,5,1))
+        >>> rng = np.random.Generator(np.random.PCG64(7))
+        >>> kmt = KernelMultiTask(kernel_base,
+        ...     num_tasks=4,
+        ...     shape_factor=(6,3,5,4,2),
+        ...     diag = rng.uniform(low=0,high=1,size=(3,5,4)))
+        >>> x = np.random.rand(8,10)
+        >>> task = np.arange(4) 
+        >>> kmt(x[0],x[0],task[0],task[0]).shape
+        (6, 3, 5)
+        >>> kmt(x[0],x[0],task,task).shape
+        (6, 3, 5, 4)
+        >>> kmt(x,x,task[0],task[0]).shape
+        (6, 3, 5, 8)
+        >>> v = kmt(x[:4,None,:],x[None,:4,:],task,task)
+        >>> v.shape
+        (6, 3, 5, 4, 4)
+        >>> kmat_x = kmt.base_kernel(x[:4,None,:],x[None,:4,:])
+        >>> kmat_x.shape
+        (3, 5, 4, 4)
+        >>> kmat_tasks = kmt.taskmat[...,task,task]
+        >>> kmat_tasks.shape
+        (6, 3, 5, 4)
+        >>> np.allclose(v,kmat_tasks[...,None,:]*kmat_x)
+        True
+        >>> np.allclose(v,kmat_tasks[...,:,None]*kmat_x)
+        False
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None,None],task[None,:,None,None]).shape
+        (6, 3, 5, 4, 4, 8, 8)
+        >>> kmt(x[:,None,None,None,:],x[None,:,None,None,:],task[:,None],task[None,:]).shape
+        (6, 3, 5, 8, 8, 4, 4)
+
+        PyTorch
+        
+        >>> import torch 
+        >>> kmt = KernelMultiTask(KernelGaussian(d=2,torchify=True),num_tasks=3,diag=[1,2,3])
+        >>> x = torch.from_numpy(np.random.rand(4,2))
+        >>> task = np.arange(3) 
+        >>> kmt(x[0],x[0],task[1],task[1]).item()
+        3.0
+        >>> kmt(x[0],x[0],task,task)
+        array([2., 3., 4.])
+        >>> kmt(x[0],x[0],task[:,None],task[None,:]).shape
+        (3, 3)
+        >>> kmt(x,x,task[1],task[1])
+        array([3., 3., 3., 3.])
+        >>> kmt(x[:3],x[:3],task,task)
+        array([2., 3., 4.])
+        >>> kmt(x,x,task[:,None],task[:,None]).shape
+        (3, 4)
+        >>> v = kmt(x[:3,None,:],x[None,:3,:],task,task)
+        >>> v.shape
+        (3, 3)
+        >>> torch.allclose(v,kmt.base_kernel(x[:3,None,:],x[None,:3,:])*kmt.taskmat[task,task])
+        True
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None],task[:,None,None]).shape
+        (3, 4, 4)
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None,None],task[None,:,None,None]).shape
+        (3, 3, 4, 4)
+
+        Batched inference
+
+        >>> kernel_base = KernelGaussian(d=10,shape_lengthscales=(5,1),shape_scale=(3,5,1),torchify=True)
+        >>> rng = np.random.Generator(np.random.PCG64(7))
+        >>> kmt = KernelMultiTask(kernel_base,
+        ...     num_tasks=4,
+        ...     shape_factor=(6,3,5,4,2),
+        ...     diag = rng.uniform(low=0,high=1,size=(3,5,4)))
+        >>> x = torch.from_numpy(np.random.rand(8,10))
+        >>> task = torch.arange(4) 
+        >>> kmt(x[0],x[0],task[0],task[0]).shape
+        (6, 3, 5)
+        >>> kmt(x[0],x[0],task,task).shape
+        (6, 3, 5, 4)
+        >>> kmt(x,x,task[0],task[0]).shape
+        (6, 3, 5, 8)
+        >>> v = kmt(x[:4,None,:],x[None,:4,:],task,task)
+        >>> v.shape
+        (6, 3, 5, 4, 4)
+        >>> kmat_x = kmt.base_kernel(x[:4,None,:],x[None,:4,:])
+        >>> kmat_x.shape
+        (3, 5, 4, 4)
+        >>> kmat_tasks = kmt.taskmat[...,task,task]
+        >>> kmat_tasks.shape
+        (6, 3, 5, 4)
+        >>> torch.allclose(v,kmat_tasks[...,None,:]*kmat_x)
+        True
+        >>> torch.allclose(v,kmat_tasks[...,:,None]*kmat_x)
+        False
+        >>> kmt(x[:,None,:],x[None,:,:],task[:,None,None,None],task[None,:,None,None]).shape
+        (6, 3, 5, 4, 4, 8, 8)
+        >>> kmt(x[:,None,None,None,:],x[None,:,None,None,:],task[:,None],task[None,:]).shape
+        (6, 3, 5, 8, 8, 4, 4)
     """
 
     def __init__(self,
@@ -42,7 +165,9 @@ class KernelMultiTask(AbstractKernel):
             requires_grad_diag (bool): If `True` and `torchify`, set `requires_grad=True` for `diag`.
         """
         assert isinstance(base_kernel,AbstractKernel)
-        super().__init__(d=base_kernel.d,shape_batch=base_kernel.shape_batch,torchify=base_kernel.torchify,device=base_kernel.device)
+        super().__init__(d=base_kernel.d,torchify=base_kernel.torchify,device=base_kernel.device)
+        self.base_kernel = base_kernel
+        self.AUTOGRADKERNEL = base_kernel.AUTOGRADKERNEL 
         assert np.isscalar(num_tasks) and num_tasks%1==0 and num_tasks>1
         self.num_tasks = num_tasks
         assert np.isscalar(rank_factor) and rank_factor%1==0 and 1<=rank_factor<=self.num_tasks
@@ -55,7 +180,6 @@ class KernelMultiTask(AbstractKernel):
             endsize_ops = list(range(self.num_tasks+1)),
             constraints = [])
         assert self.raw_factor.shape[-2]==self.num_tasks
-        self.batch_params["factor"] = self.factor
         self.raw_diag,self.tf_diag = self.parse_assign_param(
             pname = "diag",
             param = diag, 
@@ -64,7 +188,9 @@ class KernelMultiTask(AbstractKernel):
             tfs_param = tfs_diag,
             endsize_ops = [1,self.num_tasks],
             constraints = ["POSITIVE"])
-        self.batch_params["diag"] = self.diag
+        self.eye_num_tasks = self.npt.eye(self.num_tasks,**self.nptkwargs)
+        self.batch_params["taskmat"] = self.taskmat
+        self.nbdim_base = self.base_kernel.nbdim
     
     @property
     def factor(self):
@@ -73,6 +199,39 @@ class KernelMultiTask(AbstractKernel):
     @property
     def diag(self):
         return self.tf_diag(self.raw_diag)
+
+    @property
+    def taskmat(self):
+        factor = self.factor
+        diag = self.diag
+        taskmat = self.npt.einsum("...ij,...kj->...ik",factor,factor)+diag[...,None]*self.eye_num_tasks
+        return taskmat
     
-    def __call__(self, *args, **kwargs):
-        assert False, "not implemented"
+    def __call__(self, x0, x1, task0, task1, beta0=None, beta1=None):
+        r"""
+        Evaluate the kernel with (optional) partial derivatives 
+
+        $$\partial_{\boldsymbol{x}_0}^{\boldsymbol{\beta}_0} \partial_{\boldsymbol{x}_1}^{\boldsymbol{\beta}_1} K((\boldsymbol{x}_0,i),(\boldsymbol{x}_1,j)).$$
+        
+        Args:
+            x0 (Union[np.ndarray,torch.Tensor]): Shape `x0.shape=(...,d)` first input to kernel.
+            x1 (Union[np.ndarray,torch.Tensor]): Shape `x1.shape=(...,d)` second input to kernel. 
+            task0 (Union[int,np.ndarray,torch.Tensor]): First task indices.. 
+            task1 (Union[int,np.ndarray,torch.Tensor]): Second task indices.
+            beta0 (Union[np.ndarray,torch.Tensor]): Shape `beta0.shape==(d,)` derivative orders with respect to first inputs, $\boldsymbol{\beta}_0$.
+            beta1 (Union[np.ndarray,torch.Tensor]): Shape `beta1.shape==(d,)` derivative orders with respect to first inputs, $\boldsymbol{\beta}_1$.
+        
+        Returns:
+            k (Union[np.ndarray,torch.Tensor]): Shape `y.shape=(x0+x1).shape[:-1]` kernel evaluations. 
+        """
+        kmat_x = self.base_kernel.__call__(x0,x1,beta0,beta1)[...,None]
+        nnbdim_x = kmat_x.ndim-self.nbdim_base-1
+        kmat_tasks = self.taskmat
+        nbdim = kmat_tasks.ndim-2
+        kmat_tasks = kmat_tasks[...,task0,task1,None]
+        nnbdim_kmat = kmat_tasks.ndim-nbdim-1
+        commondim = max(nnbdim_x,nnbdim_kmat)
+        kmat_x = insert_batch_dims(kmat_x,commondim-nnbdim_x,self.nbdim_base)
+        kmat_tasks = insert_batch_dims(kmat_tasks,commondim-nnbdim_kmat,nbdim)
+        kmat = kmat_x*kmat_tasks 
+        return kmat[...,0]
