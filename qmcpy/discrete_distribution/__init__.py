b52f0d7c9e47c5d5c282d99f419d15fba8efc7a4
CDHJ2000 <cdhjr2000@gmail.com>
2025-07-24 18:09:04 -0500

    Kronecker

--- qmcpy/discrete_distribution/__init__.py
+++ qmcpy/discrete_distribution/__init__.py
@@ -3,14 +3,4 @@ from .iid_std_uniform import IIDStdUniform
 from .lattice import Lattice
 from .digital_net_b2 import DigitalNetB2
 from .halton import Halton
-<<<<<<< HEAD
 from .kronecker import Kronecker
-=======
-
-DiscreteDistribution = AbstractDiscreteDistribution
-_DiscreteDistribution = AbstractDiscreteDistribution
-Sobol = DigitalNetB2
-DigitalNet = DigitalNetB2
-Net = DigitalNetB2
-
->>>>>>> 29e737d328af14af2ef61bb435db42f293daf824
