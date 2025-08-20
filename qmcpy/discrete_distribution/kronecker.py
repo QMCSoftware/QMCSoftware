76545dd4442586a0b848716d5d5b727b56acb2c5
CDHJ2000 <cdhjr2000@gmail.com>
2025-08-19 20:50:52 -0500

    Fixing the Kronecker code

--- qmcpy/discrete_distribution/kronecker.py
+++ qmcpy/discrete_distribution/kronecker.py
@@ -15,7 +15,7 @@ PRIMES = array([2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41,
 RICHTMYER = sqrt(PRIMES) % 1
 
 class Kronecker(AbstractIIDDiscreteDistribution):
-    def __init__(self, dimension = 1, alpha = 0, delta = 0, replications = 1, randomize = False, seed = None, m_max = 2 ** 21, d_max = None):
+    def __init__(self, dimension=1, replications=1, randomize=False, alpha = 0, delta = 0, seed_alpha=None, seed = None, order='natural', d_max=None, m_max=None):
         # attributes required for cub_qmc_clt.py
         self.mimics = 'StdUniform'
         self.d = dimension
@@ -24,12 +24,16 @@ class Kronecker(AbstractIIDDiscreteDistribution):
         self.dimension = dimension
         self.low_discrepancy = True
         self.d_max = dimension
-        self.m_max = m_max
+        self.m_max = int(1e10)
+        # self.order = order
         
-        if type(alpha) == str and alpha.lower() == 'richtmyer':
+        # plain string
+        if type(alpha) == list and type(alpha[0]) == str:
+            if alpha[0].lower() == 'richtmyer':
                 if dimension <= len(PRIMES):
                     self.alpha = RICHTMYER[:dimension]
                 else:
+                    print(len(RICHTMYER))
                     self.alpha = append(RICHTMYER, [sqrt(nextprime(PRIMES[-1], ith=x)) % 1 for x in range(1, dimension - len(PRIMES) + 1)])
         else:
             if sum(alpha) == 0:
@@ -37,55 +41,32 @@ class Kronecker(AbstractIIDDiscreteDistribution):
             else:
                 self.alpha = alpha
 
-        super(Kronecker,self).__init__(dimension,seed)
-
-        if self.randomize:
-            self.delta = random.rand(self.replications, self.dimension)
-        elif sum(delta) == 0 and seed == None:
+        if sum(delta) == 0 and seed == None:
             self.delta = zeros(dimension)
         elif sum(delta) == 0 and seed != None:
             self.delta = random.rand(dimension)
         elif sum(delta) != 0:
             self.delta = delta
-        else:
-            self.delta = 0
-
+            
+        super(Kronecker,self).__init__(dimension,seed)
+    
 
     def gen_samples(self, n=None, n_min=0, n_max=0):
-        # returns replications x (n_max-n_min) x d (dimension) array of samples
         if n is None:
             n = n_max - n_min
 
         i = arange(n).reshape((n, 1))
 
-        if self.replications == 1:
-            points = (i * self.alpha) + self.delta
-            return subtract(points, floor(points), out = points)
+        if self.randomize:
+            # different for each component
+            delta = random.rand(1, self.dimension)
+        else:
+            delta = self.delta
 
-        samples = empty(shape=(self.replications, n, self.dimension))
-        for r in range(self.replications):
-            samples[r] = ((i * self.alpha) + self.delta[r]) % 1
-
-        return samples
-
+        return ((i * self.alpha) + delta) % 1
+    
 
     def periodic_discrepancy(self, n, k_tilde=None, gamma=None):
-        """
-        Calculates the discrepancy for a periodic kernel.
-
-        Args:
-            n (int): the number of sample points
-            k_tilde (tuple(function, float)): the function takes in 2 arguments: the sample points and the coordinate weights.
-                The float is the integral over the unit hypercube.
-            gamme (ndarray): shape (1xd)
-
-        Returns:
-            float
-        
-        Note:
-            If k_tilde is not specified, the second Bernoulli polynomial is used.
-            If gamma is not specified, the coordinate weights will be just all ones.
-        """
         if gamma is None:
             gamma = ones(self.dimension)
 
@@ -93,20 +74,7 @@ class Kronecker(AbstractIIDDiscreteDistribution):
             k_tilde = (lambda x, gamma: prod(1 + (x * (x - 1) + 1/6) * gamma, axis=1), 1)
 
         return sqrt(self._square_periodic_discrepancies(n, k_tilde, gamma))
-        
-
-    # calculates the weighted sum of square discrepancy
-    def wssd_discrepancy(self, n, weights, k_tilde = None, gamma = None):
-        if gamma is None:
-            gamma = ones(self.dimension)
-
-        if k_tilde is None:
-            k_tilde = (lambda x, gamma: prod(1 + (x * (x - 1) + 1/6) * gamma, axis=1), 1)
-
-        discrepancies = self._square_periodic_discrepancies(n, k_tilde, gamma)
-        return sum(weights * discrepancies)
-    
-    
+       
     def _square_periodic_discrepancies(self, n, k_tilde, gamma):
         n_array = arange(1, n + 1)
         k_tilde_terms = k_tilde[0](self.gen_samples(n=n), gamma)
@@ -118,14 +86,3 @@ class Kronecker(AbstractIIDDiscreteDistribution):
         summation = zeros(n)
         summation[1:] = left_sum - right_sum
         return (k_tilde_zero_terms + 2 * summation) / (n_array ** 2) - k_tilde[1]
-    
-    
-    def _spawn(self, child_seed, dimension):
-        return Kronecker(
-                dimension=dimension,
-                alpha = self.alpha,
-                randomize=self.randomize,
-                seed=child_seed,
-                d_max=self.d_max,
-                m_max=self.m_max,
-                replications=self.replications)
