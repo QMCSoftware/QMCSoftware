479785a4745e3cb0f29a2bd8e8eb4a7cb86eb74e
Sou-Cheng Choi <terrya@192.168.1.218>
2025-09-07 11:11:02 +0800

    Better latex table

--- demos/GBM/latex_util.py
+++ demos/GBM/latex_util.py
@@ -24,16 +24,28 @@ def format_results_dataframe(df, numeric_columns):
 
 def generate_latex_table(df, caption, label):
     """Generate LaTeX table with booktabs formatting"""
+    # Create custom header
+    header = "Method & Sampler & Mean & Std Dev & Mean  & Std Dev  & Mean Time (s)  & Std Dev (s) & Speedup \\\\\n &  &  &   &  Error &  Error &   & &  \\\\"
+    
     latex_table = df.style.hide(axis='index').to_latex(
         caption=caption,
         label=label,
-        position="btp",
+        position="tbp",
         hrules=True,
         column_format="ll@{\hspace{0.4em}}r@{\hspace{0.4em}}r@{\hspace{0.4em}}r@{\hspace{0.4em}}r@{\hspace{0.4em}}r@{\hspace{0.4em}}r@{\hspace{0.4em}}r"
     )
     # Replace default LaTeX table environment with booktabs format
-    latex_table = latex_table.replace("\\begin{table}[H]", "\\begin{table}[btp]")
+    latex_table = latex_table.replace("\\begin{table}[H]", "\\begin{table}[btp]\centering")
     latex_table = latex_table.replace("\\hline", "\\toprule", 1)
     latex_table = latex_table.replace("\\hline", "\\midrule", 1)
     latex_table = latex_table.replace("\\hline", "\\bottomrule")
-    return latex_table
+    
+    # Replace the generated header with custom header
+    lines = latex_table.split('\n')
+    for i, line in enumerate(lines):
+        if '\\toprule' in line and i + 1 < len(lines):
+            # Replace the line after \toprule with custom header
+            lines[i + 1] = header
+            break
+    
+    return '\n'.join(lines)
