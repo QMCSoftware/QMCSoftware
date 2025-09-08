2f1cbe4a6132f04b0d5b71dc3e8d36539d994c6f
Sou-Cheng Choi <terrya@192.168.1.218>
2025-09-08 13:49:24 +0800

    Fix booktests

--- test/booktests/tb_dakota_genz.py
+++ test/booktests/tb_dakota_genz.py
@@ -4,7 +4,7 @@ from __init__ import TB_TIMEOUT, BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
 
-    @testbook('../../demos/dakota_genz.ipynb', execute=True, timeout=TB_TIMEOUT)
+    @testbook('../../demos/DAKOTA_Genz/dakota_genz.ipynb', execute=True, timeout=TB_TIMEOUT)
     def test_dakota_genz_notebook(self, tb):
         pass
 
