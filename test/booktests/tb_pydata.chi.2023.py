9d12163382ea3f58d6ea74e2fce3b5c6b3fe0b81
Sou-Cheng Choi <terrya@192.168.1.218>
2025-09-08 15:09:28 +0800

    Fix booktests

---
+++ test/booktests/tb_pydata.chi.2023.py
@@ -0,0 +1,13 @@
+import unittest
+from testbook import testbook
+from __init__ import TB_TIMEOUT, BaseNotebookTest
+
+class NotebookTests(BaseNotebookTest):
+
+    @testbook('../../demos/talk_paper_demos/pydata.chi.2023.ipynb', execute=True, timeout=TB_TIMEOUT)
+    def test_pydata_chi_2023_notebook(self, tb):
+        """Test that the notebook pydata.chi.2023.ipynb executes without errors."""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
--- test/booktests/tb_why_add_q_to_mc_blog.py
+++ test/booktests/tb_why_add_q_to_mc_blog.py
@@ -3,7 +3,7 @@ from testbook import testbook
 from __init__ import TB_TIMEOUT, BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
-    @testbook('../../demos/why_add_q_to_mc_blog.ipynb', execute=True, timeout=TB_TIMEOUT)
+    @testbook('../../demos/talk_paper_demos/why_add_q_to_mc_blog/why_add_q_to_mc_blog.ipynb', execute=True, timeout=TB_TIMEOUT)
     def test_why_add_q_to_mc_blog_notebook(self, tb):
         pass
 
