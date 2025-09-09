b2c3ddbfbae5ec883dcc8eff35bf7f79359d4013
Sou-Cheng Choi <terrya@192.168.1.218>
2025-09-09 15:00:16 +0800

    Fix test/booktests/tb_pydata_chi_2023.py

--- test/booktests/tb_pydata_chi_2023.py
+++ test/booktests/tb_pydata_chi_2023.py
@@ -1,12 +1,12 @@
 import unittest
-from testbook import testbook
+from testbook import testbook 
 from __init__ import TB_TIMEOUT, BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
 
     @testbook('../../demos/talk_paper_demos/pydata_chi_2023.ipynb', execute=True, timeout=TB_TIMEOUT)
     def test_pydata_chi_2023_notebook(self, tb):
-        """Test that the notebook pydata.chi.2023.ipynb executes without errors."""
+        """Test that the notebook pydata_chi_2023.ipynb executes without errors."""
         pass
 
 if __name__ == '__main__':
