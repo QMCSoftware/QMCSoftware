19e2f450cdcdbd990c04bf0bb8cd399ca87850e3
Larysa Matiukha <123220119+larissensium@users.noreply.github.com>
2025-12-06 23:54:46 -0600

    averaged MAE for qmcpy + quantlib

--- demos/GBM/averaged_mae.py
+++ demos/GBM/averaged_mae.py
@@ -1,68 +1,110 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from qmcpy_util_replications import generate_qmcpy_paths
+import quantlib_util as qlu
+import qmcpy_util_replications as qpru
+import qmcpy_util as qpu
 import config as cf
 import plot_util as pu
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
-def compute_mae_vs_paths(sampler_type='Sobol',
-                                replications=5):
+def compute_mae_vs_paths(method, sampler, replications=5, qp_seed = 42, ql_seed=7):
     """
-    Compute mean absolute error vs number of paths using independent QMCPy replications
+    Compute averaged MAE vs number of paths for all samplers
     """
-
-    # Get experiment configuration 
+    # Experiment configuration
     exp_cfg = cf.get_experiment_configurations()['paths']
     n_paths_range = exp_cfg['range']
     n_steps = exp_cfg['fixed_steps']
 
-    # Get GBM parameters
+    # GBM parameters
     gbm_params = cf.get_gbm_parameters()
     initial_value = gbm_params['initial_value']
     mu = gbm_params['mu']
-    diffusion = gbm_params['sigma']**2 # diffusion = sigma^2
+    sigma = gbm_params['sigma']
+    diffusion = sigma**2
     maturity = gbm_params['maturity']
 
-    # Compute theoretical mean
+    # Theoretical mean
     theoretical_mean = initial_value * np.exp(mu * maturity)
-    
+
     mean_errors = []
 
     for n_paths in n_paths_range:
-        # Generate GBM paths (with idependant replications) for each number of paths in n_paths_range
-        paths, gbm = generate_qmcpy_paths(
-            initial_value=initial_value,
-            mu=mu,
-            diffusion=diffusion,
-            maturity=maturity,
-            n_steps=n_steps,
-            n_paths=n_paths,
-            sampler_type=sampler_type,
-            replications=replications,
-        )
+        if method == "QMCPy":
+            # errors = []
+            # for r in range(replications):
+            #     seed = qp_seed + r
+            #     paths, _ = qpu.generate_qmcpy_paths(
+            #         initial_value = initial_value,
+            #         mu=mu,
+            #         diffusion=diffusion,
+            #         maturity=maturity,
+            #         n_steps=n_steps,
+            #         n_paths=n_paths,
+            #         sampler_type=sampler,
+            #         seed=seed
+            #     )
+            #     final_vals = paths[:, -1]
+            #     errors.append(np.abs(final_vals.mean() - theoretical_mean))
 
-        # Extract values at the final time
-        final_vals = paths[:, :, -1]  
+            paths, _ = qpru.generate_qmcpy_paths(
+                initial_value=initial_value,
+                mu=mu,
+                diffusion=diffusion,
+                maturity=maturity,
+                n_steps=n_steps,
+                n_paths=n_paths,
+                sampler_type=sampler,
+                replications=replications
+            )
+            # Extract values at the final time
+            final_vals = paths[:, :, -1]  
+            # Compute MAE for each replication and take the average
+            errors = np.abs(final_vals.mean(axis=1) - theoretical_mean)
 
-        # Compute MAE for each replication and take the average
-        errors_per_repl = np.abs(final_vals.mean(axis=1) - theoretical_mean)
-        mean_errors.append(errors_per_repl.mean())
+        elif method == "QuantLib":
+            errors = []
+            for r in range(replications):
+                seed = ql_seed + r
+                paths, _ = qlu.generate_quantlib_paths(
+                    initial_value=initial_value,
+                    mu=mu,
+                    sigma=sigma,
+                    maturity=maturity,
+                    n_steps=n_steps,
+                    n_paths=n_paths,
+                    sampler_type=sampler,
+                    seed=seed
+                )
+                final_vals = paths[:, -1]
+                errors.append(np.abs(final_vals.mean() - theoretical_mean))
+
+        mean_errors.append(np.mean(errors))
 
     return n_paths_range, mean_errors
 
-def plot_mae_vs_paths(replications):
 
+def plot_mae_vs_paths(replications=5):
+    """
+    Plot averaged MAE vs number of paths for all samplers
+    """
     styling = pu.get_plot_styling()
-    colors = styling['colors']['QMCPy']
-    markers = styling['markers']['QMCPy']
-    samplers = cf.get_sampler_configurations()['all_samplers'] 
+    sampler_cfg = cf.get_sampler_configurations()
+    qmcpy_samplers = sampler_cfg['all_samplers']
+    ql_samplers = sampler_cfg['quantlib_samplers']
 
-    fig, ax = plt.subplots(figsize=(10,6))
+    # list of (method, sample) pairs to plot
+    all_pairs = [("QMCPy", s) for s in qmcpy_samplers] + [("QuantLib", s) for s in ql_samplers]
 
-    for sampler in samplers:
-        n_paths_range, mean_errors = compute_mae_vs_paths(sampler_type=sampler, replications=replications)
-        
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    for method, sampler in all_pairs:
+        n_paths_range, mean_errors = compute_mae_vs_paths(method, sampler, replications)
+
+        colors = styling['colors'][method]
+        markers = styling['markers'][method]
+
         ax.loglog(
             n_paths_range,
             mean_errors,
@@ -70,52 +112,18 @@ def plot_mae_vs_paths(replications):
             color=colors.get(sampler),
             linewidth=2,
             markersize=6,
-            label=sampler
+            label=f"{method} - {sampler}"
         )
 
     ax.xaxis.set_major_locator(FixedLocator(n_paths_range))
     ax.xaxis.set_major_formatter(FixedFormatter([str(int(x)) for x in n_paths_range]))
-    ax.xaxis.set_minor_locator(FixedLocator([]))  
-
+    ax.xaxis.set_minor_locator(FixedLocator([]))
 
     ax.set_xlabel("Number of Paths", fontsize=12, fontweight='bold')
     ax.set_ylabel("Mean Absolute Error", fontsize=12, fontweight='bold')
-    ax.set_title(f"MAE vs Number of Paths across {replications} replications (n_steps = 252)", fontsize=14, fontweight='bold')
+    ax.set_title(f"MAE vs Number of Paths across {replications} replications (n_steps = 252)",
+                 fontsize=14, fontweight='bold')
     ax.grid(True, alpha=0.3)
     ax.legend(fontsize=10)
 
     plt.show()
-
-    #old version
-def compute_mean_error_vs_paths(n_paths_list, sampler_type='Sobol', n_steps=252,
-                                replications=5, initial_value=100, mu=0.05,
-                                diffusion=0.2**2, maturity=1.0, seed=42):
-    """
-    Compute mean absolute error vs number of paths using independent QMCPy replications.
-    """
-
-
-    theo_mean = initial_value * np.exp(mu * maturity)
-    mean_errors = []
-
-    for n_paths in n_paths_list:
-        # Generate paths with replications
-        paths, gbm = generate_qmcpy_paths(
-            initial_value=initial_value,
-            mu=mu,
-            diffusion=diffusion,
-            maturity=maturity,
-            n_steps=n_steps,
-            n_paths=n_paths,
-            sampler_type=sampler_type,
-            replications=replications,
-            seed=seed
-        )
-
-        final_vals = paths[:, :, -1]
-
-        errors_per_repl = np.abs(final_vals.mean(axis=1) - theo_mean)
-
-        mean_errors.append(errors_per_repl.mean())
-
-    return mean_errors
