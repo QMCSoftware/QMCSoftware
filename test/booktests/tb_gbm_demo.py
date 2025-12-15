9186d4a7483d88e63790cc4d3658206a2e63cea2
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2025-12-15 15:59:53 -0600

    Increase timeout. Add nbconvert.

--- test/booktests/tb_gbm_demo.py
+++ test/booktests/tb_gbm_demo.py
@@ -8,7 +8,7 @@ class NotebookTests(BaseNotebookTest):
         fix_gbm_symlinks(notebook_dir)
         self.execute_notebook_file(notebook_path, timeout=600)
         # Toggle code cell [3] cf.is_debug -> True before executing
-        run_notebook(notebook_path, notebook_dir, change_value=True)
+        run_notebook(notebook_path, notebook_dir, change_value=True, timeout=600)
         
         
 if __name__ == '__main__':
