cfbe5ec988d604aa20b0deae529c9d0ee473457a
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2026-07-17 10:36:13 +0800

    Replace with a smaller example in code cell [5]

--- test/booktests/tb_lattice_kronecker_methods.py
+++ test/booktests/tb_lattice_kronecker_methods.py
@@ -1,10 +1,21 @@
 import unittest
 from __init__ import BaseNotebookTest
 
+
 class NotebookTests(BaseNotebookTest):
 
     def test_lattice_kronecker_methods_notebook(self):
-        self.run_notebook('../../demos/lattice_kronecker_methods.ipynb')
+        # Keep enough lattice candidates for the reduced dimension: dim <= n / 4.
+        replacements = {
+            "dim = 64": "dim = 8",
+            "n = 2**20": "n = 2**5",
+            "searchsize = 25": "searchsize = 4",
+        }
+        self.run_notebook(
+            "../../demos/lattice_kronecker_methods.ipynb",
+            replacements=replacements,
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
