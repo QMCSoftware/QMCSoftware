15ee863c9ea855f7f80f47aa943b6ee86857f2c6
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2026-07-17 10:16:33 +0800

    Add imports in __init__.py

--- qmcpy/discrete_distribution/__init__.py
+++ qmcpy/discrete_distribution/__init__.py
@@ -1,10 +1,10 @@
 from .abstract_discrete_distribution import AbstractDiscreteDistribution
 from .iid_std_uniform import IIDStdUniform
-from .lattice import Lattice
+from .lattice import Lattice, lattice_vector_wssd_search
 from .digital_net_b2 import DigitalNetB2
 from .digital_net_any_bases import DigitalNetAnyBases,Halton,Faure
 from .mpmc import MPMC
-from .kronecker import Kronecker
+from .kronecker import Kronecker, kronecker_search_march_2026
 
 DiscreteDistribution = AbstractDiscreteDistribution
 _DiscreteDistribution = AbstractDiscreteDistribution
@@ -12,4 +12,3 @@ Sobol = DigitalNetB2
 DigitalNet = DigitalNetB2
 Net = DigitalNetB2
 NetB2 = DigitalNetB2
-
