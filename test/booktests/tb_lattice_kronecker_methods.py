a48c42e7b777da09e6d32818f7e0741b603bc6d7
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2026-07-17 10:33:14 +0800

    Fix unit test for demo

--- test/booktests/tb_lattice_kronecker_methods.py
+++ test/booktests/tb_lattice_kronecker_methods.py
@@ -1,12 +1,10 @@
 import unittest
-from testbook import testbook
-from __init__ import TB_TIMEOUT, BaseNotebookTest
+from __init__ import BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
 
-    @testbook('../../demos/lattice_kronecker_methods.ipynb', execute=True, timeout=TB_TIMEOUT)
-    def test_lattice_kronecker_methods_notebook(self, tb):
-        pass
+    def test_lattice_kronecker_methods_notebook(self):
+        self.run_notebook('../../demos/lattice_kronecker_methods.ipynb')
 
 if __name__ == '__main__':
     unittest.main()
