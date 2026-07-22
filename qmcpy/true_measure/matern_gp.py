b3822db3d8ee93884a9794a98e13d6113ec67698
ishn29 <ishaan.hn@gmail.com>
2026-07-21 19:33:20 -0500

    Fix MaternGP spawning and preserve kernel parameters

--- qmcpy/true_measure/matern_gp.py
+++ qmcpy/true_measure/matern_gp.py
@@ -123,7 +123,8 @@ class MaternGP(Gaussian):
         self.points = points
         self.length_scale = length_scale
         self.nu = nu
-        self.variance = variance
+        self._kernel_variance = variance
+        self.nugget = nugget
         dists = np.linalg.norm(
             points[..., :, None, :] - points[..., None, :, :], axis=-1
         )
@@ -149,13 +150,20 @@ class MaternGP(Gaussian):
             sampler, mean=mean, covariance=covariance, decomp_type=decomp_type
         )
 
-    def _spawn(self, sampler):
+    def _spawn(self, sampler, dimension=None):
+        dimension = sampler.d if dimension is None else dimension
+        if dimension != self.d:
+            raise DimensionError(
+                "MaternGP cannot be spawned with a different dimension because "
+                "its dimension is fixed by the number of points."
+            )
         return MaternGP(
             sampler,
             self.points,
             length_scale=self.length_scale,
             nu=self.nu,
-            variance=self.variance,
+            variance=self._kernel_variance,
             mean=self.mean,
+            nugget=self.nugget,
             decomp_type=self.decomp_type,
         )
