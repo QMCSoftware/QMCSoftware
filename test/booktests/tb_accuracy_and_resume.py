83cb523d6b8adbdeb36f05cf52ad67b6d59e3e5a
sou-cheng-choi <sou.cheng.choi@stanfordalumni.org>
2026-08-09 11:54:30 +0800

    Restore test files from develop branch

--- test/booktests/tb_accuracy_and_resume.py
+++ test/booktests/tb_accuracy_and_resume.py
@@ -1,12 +1,17 @@
 import unittest
-from testbook import testbook
-from __init__ import TB_TIMEOUT, BaseNotebookTest
+from __init__ import BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
 
-    @testbook('../../demos/demo_resume_data/accuracy_and_resume.ipynb', execute=True, timeout=TB_TIMEOUT)
-    def test_accuracy_and_resume_notebook(self, tb):
-        pass
+    def test_accuracy_and_resume_notebook(self):
+        notebook_path, notebook_dir = self.locate_notebook(
+            "../../demos/demo_resume_data/accuracy_and_resume.ipynb"
+        )
+        symlinks_to_fix = [
+            "resume_util.py",
+        ]
+        self.fix_symlinks(notebook_dir, symlinks_to_fix)
+        self.run_notebook(notebook_path)
 
 if __name__ == '__main__':
     unittest.main()
--- test/booktests/tb_resume_examples.py
+++ test/booktests/tb_resume_examples.py
@@ -1,12 +1,17 @@
 import unittest
-from testbook import testbook
-from __init__ import TB_TIMEOUT, BaseNotebookTest
+from __init__ import BaseNotebookTest
 
 class NotebookTests(BaseNotebookTest):
 
-    @testbook('../../demos/demo_resume_data/resume_examples.ipynb', execute=True, timeout=TB_TIMEOUT)
-    def test_resume_examples_notebook(self, tb):
-        pass
+    def test_resume_examples_notebook(self):
+        notebook_path, notebook_dir = self.locate_notebook(
+            "../../demos/demo_resume_data/resume_examples.ipynb"
+        )
+        symlinks_to_fix = [
+            "resume_util.py",
+        ]
+        self.fix_symlinks(notebook_dir, symlinks_to_fix)
+        self.run_notebook(notebook_path)
 
 if __name__ == '__main__':
     unittest.main()
